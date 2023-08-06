c0dbd111cc7457bd5fcade9c97c4eb7dd6724b5f
Drew Daniels <drew.daniels@webpt.com>
2023-08-06 17:30:17 -0500

    add cucumber lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,7 @@ local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup {}
 lspconfig.eslint.setup {}
 lspconfig.solargraph.setup {}
+lspconfig.cucumber_language_server.setup {}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
