fcf13fbe239c6321394a45b8f159118d9a932196
Drew Daniels <daniels_drew@protonmail.com>
2023-08-07 20:55:40 -0500

    add tmux config

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/drew.daniels/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/drew.daniels/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/drew.daniels/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/drew.daniels/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/drew.daniels/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/drew/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/drew/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/drew/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/drew/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/drew/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,67 +76,67 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   fzf = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/fzf",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/fzf",
     url = "https://github.com/junegunn/fzf"
   },
   ["fzf.vim"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/fzf.vim",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/fzf.vim",
     url = "https://github.com/junegunn/fzf.vim"
   },
   ["gv.vim"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/gv.vim",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/gv.vim",
     url = "https://github.com/junegunn/gv.vim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["vim-endwise"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/vim-endwise",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/vim-endwise",
     url = "https://github.com/tpope/vim-endwise"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-rhubarb"] = {
     loaded = true,
-    path = "/Users/drew.daniels/.local/share/nvim/site/pack/packer/start/vim-rhubarb",
+    path = "/Users/drew/.local/share/nvim/site/pack/packer/start/vim-rhubarb",
     url = "https://github.com/tpope/vim-rhubarb"
   }
 }
