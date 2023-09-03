1708d590733897a2c2974988d3f3c43c92e62472
Drew Daniels <daniels_drew@protonmail.com>
2023-09-03 11:29:00 -0500

    add github copilot plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,6 +7,7 @@ return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'joshdick/onedark.vim'
   use 'sheerun/vim-polyglot'
+  use 'github/copilot.vim'
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
