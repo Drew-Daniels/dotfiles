4f00334e6ae35e3382ebd95cb049a66a545eb577
Drew Daniels <drew.daniels@webpt.com>
2023-09-06 16:36:17 -0500

    use recommended startup setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -141,4 +141,10 @@ vim.cmd([[
     let g:gutentags_project_root = ['Gemfile']
 ]])
 vim.cmd([[colorscheme onedark]])
-vim.cmd([[NvimTreeOpen]])
+
+local function open_nvim_tree()
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
