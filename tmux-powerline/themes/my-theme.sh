63ab01446d535285485c7020d0ac9649984dd32a
Drew Daniels <daniels_drew@protonmail.com>
2023-09-09 18:50:14 -0500

    update tmux-powerline theme

--- tmux-powerline/themes/my-theme.sh
+++ tmux-powerline/themes/my-theme.sh
@@ -77,37 +77,37 @@ fi
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
-		"hostname 33 0" \
-		#"ifstat 30 255" \
-		"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
-		"vcs_branch 29 88" \
-		"vcs_compare 60 255" \
-		"vcs_staged 64 255" \
-		"vcs_modified 9 255" \
-		"vcs_others 245 0" \
+		"tmux_session_info 235 82" \
+		"hostname 235 82" \
+		#"ifstat 3235 82" \
+		#"ifstat_sys 3235 82" \
+		"lan_ip 235 82" \
+		"wan_ip 235 82" \
+		"vcs_branch 235 82" \
+		"vcs_compare 235 82" \
+		"vcs_staged 235 82" \
+		"vcs_modified 235 82" \
+		"vcs_others 235 82" \
 	)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		"pwd 235 82" \
 		#"macos_notification_count 29 255" \
 		#"mailcount 9 255" \
-		"now_playing 234 37" \
-		"cpu 240 136" \
-		"load 237 167" \
-		"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
+		"now_playing 235 82" \
+		#"cpu 240 136" \
+		#"load 237 167" \
+		#"tmux_mem_cpu_load 234 136" \
+		"battery 235 82" \
 		"weather 37 255" \
-        #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+        #"rainbarf --rgb" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date_day 235 82" \
+		"date 235 82" \
+		"time 235 82" \
+		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
