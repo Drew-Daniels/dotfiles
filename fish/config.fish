24597b536a0ab07a0981eba75185bcbd2e9f9353
Drew Daniels <drew.daniels@webpt.com>
2023-09-15 16:28:04 -0500

    add pip shell completion for fish

--- fish/config.fish
+++ fish/config.fish
@@ -54,4 +54,19 @@ if status is-interactive
 
     # go
     . ~/.asdf/plugins/golang/set-env.fish
+
+    # pip fish completion start
+    function __fish_complete_pip
+        set -lx COMP_WORDS (commandline -o) ""
+        set -lx COMP_CWORD ( \
+            math (contains -i -- (commandline -t) $COMP_WORDS)-1 \
+        )
+        set -lx PIP_AUTO_COMPLETE 1
+        string split \  -- (eval $COMP_WORDS[1])
+    end
+    complete -fa "(__fish_complete_pip)" -c pip
+    # pip fish completion end
+
 end
+
+
