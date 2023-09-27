295df4f29a68ca35691972e0d74d23abd3d89e95
Drew Daniels <drew.daniels@webpt.com>
2023-09-27 17:21:58 -0500

    add more snippets

--- nvim/luasnippets/javascript.lua
+++ nvim/luasnippets/javascript.lua
@@ -1,8 +1,25 @@
 ---@diagnostic disable: undefined-global
 
+local helpers = require("luasnip-helpers")
+local get_visual = helpers.get_visual
+
 return {
 	s("rli", fmt([[ <li key={{{}}}>{{{}}}</li> ]], { i(1), i(2) })),
   s("dnl", fmt([[ // eslint-disable-next-line {} ]], { i(1, "rule-to-ignore") })),
+  s("des",
+    fmt(
+      [[
+        /* eslint-disable {} */
+        {}
+        /* eslint-enable {} */
+      ]]
+      ,
+      {
+        i(1, "rule-to-ignore"),
+        d(2, get_visual),
+        r(1, "rule-to-ignore"),
+      })
+  ),
   s("pt",
     fmt(
       [[ 
@@ -11,5 +28,7 @@ return {
         }}
       ]],
       { i(1), i(2) }
-    ))
+    )),
+  -- TODO: Make snippets for jest tests, describe blocks
+  -- TODO: Make snippet for selecting code and commenting it out
 }
