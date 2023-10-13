4ca4d12d96388822ffb4307ec7cc265c985d0eff
Drew Daniels <daniels_drew@protonmail.com>
2023-10-13 09:48:28 -0500

    add snippet file for eruby

---
+++ nvim/luasnippets/eruby.lua
@@ -0,0 +1,9 @@
+---@diagnostic disable: undefined-global
+
+local helpers = require("luasnip-helpers")
+local get_visual = helpers.get_visual
+
+return {
+  -- TODO: Make snippet for <%= %> tags
+  -- TODO: Make snippet for <% %> tags
+}
