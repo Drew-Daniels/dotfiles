c59dcd4e8089d398ba49b081a2eaec0dc7f5430c
Drew Daniels <drew.daniels@webpt.com>
2023-10-16 11:00:09 -0500

    more snippets

--- nvim/luasnippets/lua.lua
+++ nvim/luasnippets/lua.lua
@@ -1,8 +1,22 @@
 ---@diagnostic disable: undefined-global
 
 return {
-  s("tb", fmta([[ { <> }]], { i(1) }), { desc = "Lua Table" }),
-  s("tf", fmta([[ <> = <>]], { i(1), i(2) } ), { desc = "Lua Table Field" } ),
-  s("td", fmta([[--TODO: <>]], { i(2) }), { desc = "TODO Note" })
+	s("tb", fmta([[ { <> }]], { i(1) }), { desc = "Lua Table" }),
+	s("tf", fmta([[ <> = <>]], { i(1), i(2) }), { desc = "Lua Table Field" }),
+	s("td", fmta([[--TODO: <>]], { i(2) }), { desc = "TODO Note" }),
+  --TODO: Figure out why this doesn't work
+-- 	s("sn",
+-- 		fmta(
+--       [=[
+--         s("<>"),
+--           fmta(
+--             [[]],
+--             { <> },
+--             { desc = "<>" }
+--           )
+--       ]=]
+--     ),
+--     { i(1, "trigger"), i(2, "nodes"), i(3, "description") },
+--     { desc = "Snippet snippet" }
+-- 	)
 }
-
