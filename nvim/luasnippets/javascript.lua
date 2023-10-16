ffa91c47c78128c4e17077368fb5d28f977b01cb
Drew Daniels <daniels_drew@protonmail.com>
2023-10-15 21:10:48 -0500

    add choice node snippet for jest functions

--- nvim/luasnippets/javascript.lua
+++ nvim/luasnippets/javascript.lua
@@ -44,6 +44,31 @@ return {
 		),
 		{ desc = "jest describe" }
 	),
+	s(
+		"jba",
+		fmta(
+			[[ 
+        beforeAll('<>', () =>> {
+          <>
+        });
+      ]],
+			{ i(1), i(2) }
+		),
+		{ desc = "jest beforeAll" }
+	),
+	s(
+		"jbe",
+		fmta(
+			[[ 
+        beforeEach('<>', () =>> {
+          <>
+        });
+      ]],
+			{ i(1), i(2) }
+		),
+		{ desc = "jest beforeAll" }
+	),
 	s("jt", fmta([[ it('<>', () =>> {<>});]], { i(1), i(2) }), { desc = "jest test" }),
 	s("jx", fmta([[ expect(<>)<>;]], { i(1), i(0) }), { desc = "jest expect" }),
+  s("jf", fmta([[ <>('<>', () =>> {<>}); ]], { c(1, { t "describe", t "it", t "beforeAll", t "beforeEach" }), i(2), i(3) })),
 }
