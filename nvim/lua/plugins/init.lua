d994b8c23031e59b754a198d7c3aadcfe4e6862e
Drew Daniels <daniels_drew@protonmail.com>
2023-10-29 11:33:24 -0500

    add copilot plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -16,6 +16,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 return require("lazy").setup({
+  { "github/copilot.vim" },
   { "nvim-telescope/telescope-media-files.nvim", dependencies = { "nvim-lua/popup.nvim" } },
 	{
 		"piersolenski/telescope-import.nvim",
