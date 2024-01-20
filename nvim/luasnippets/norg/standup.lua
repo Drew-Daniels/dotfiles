89a49dedc13287eb7a945a2b52e2101499ca99a2
Drew Daniels <daniels_drew@protonmail.com>
2024-01-20 17:58:38 -0600

    update SU snippet

---
+++ nvim/luasnippets/norg/standup.lua
@@ -0,0 +1,34 @@
+---@diagnostic disable: undefined-global
+
+local helpers = require("luasnip-helpers")
+local get_visual = helpers.get_visual
+
+return {
+	s(
+		"su",
+		fmta(
+			[[
+        * Standup <>
+
+        ** What did I do yesterday?
+          <>
+
+        ** What am I doing today?
+          <>
+
+        ** What's blocking me?
+          <>
+
+        * Work TODO
+          - ( ) <>
+
+        * Personal TODO
+          - ( ) <>
+
+      ]],
+			{
+        i(1), i(2), i(3), i(4), i(5), i(6)
+			}
+		)
+	),
+}
