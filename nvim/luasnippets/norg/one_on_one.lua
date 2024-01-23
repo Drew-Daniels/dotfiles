38831722420c3ceb96085345122af56e7908d063
Drew Daniels <drew.daniels@webpt.com>
2024-01-23 14:36:43 -0600

    add snippet to create one on one

---
+++ nvim/luasnippets/norg/one_on_one.lua
@@ -0,0 +1,25 @@
+---@diagnostic disable: undefined-global
+
+local helpers = require("luasnip-helpers")
+local get_visual = helpers.get_visual
+
+return {
+	s(
+		"oo",
+		fmta(
+			[[
+        * One on One <>
+
+        ** Agenda
+          - <>
+
+        ** Action Items
+          - <>
+
+      ]],
+			{
+        i(1), i(2), i(3)
+			}
+		)
+	),
+}
