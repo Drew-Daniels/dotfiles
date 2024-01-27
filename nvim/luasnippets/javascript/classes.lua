1f1cd7b59775635a35e0783b31bf49200595d110
Drew Daniels <daniels_drew@protonmail.com>
2024-01-27 16:54:53 -0600

    add luasnippets for js classes

--- nvim/luasnippets/javascript/classes.lua
+++ nvim/luasnippets/javascript/classes.lua
@@ -5,13 +5,33 @@ local get_visual = helpers.get_visual
 
 return {
 	s(
-		"cl",
+		"cs",
 		fmta(
 			[[ 
         <>class <> {<>}
       ]],
 			{ c(1, { t(""), t("export ") }), i(2), i(3) },
 			{ desc = "Class" }
+		)
+	),
+	s(
+		"cc",
+		fmta(
+			[[ 
+        constructor(<><>: <>) {<>}
+      ]],
+			{ c(1, { t(""), t("private ") }), i(2), i(3), i(4) },
+			{ desc = "Constructor" }
+		)
+	),
+	s(
+		"cm",
+		fmta(
+			[[ 
+        <>(<>) {<>}
+      ]],
+			{ c(1, { t(""), t("static "), t("private ") }), i(2), i(3) },
+			{ desc = "Method" }
 		)
 	),
 }
