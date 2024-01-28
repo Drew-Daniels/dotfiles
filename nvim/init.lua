d90ec0c3ad2cad8feafa5073b0b7d53ead1c6847
Drew Daniels <daniels_drew@protonmail.com>
2024-01-27 18:38:59 -0600

    remove keybinds that I don't need

--- nvim/init.lua
+++ nvim/init.lua
@@ -1216,10 +1216,6 @@ vim.keymap.set("n", "n", "nzz", { silent = true })
 vim.keymap.set("n", "N", "Nzz", { silent = true })
 vim.keymap.set("i", "<C-b>", "<CR><ESC>kA<CR>", { silent = true })
 vim.keymap.set("i", "<C-o>", "<CR><ESC>I")
--- increment each number on the line
-vim.keymap.set("v", "+", "g<C-a>", { silent = true })
--- decrement
-vim.keymap.set("v", "-", "g<C-x>", { silent = true })
 -- do not open folds when searching for text
 vim.cmd([[set foldopen-=search]])
 -- do not open folds when moving cursor
