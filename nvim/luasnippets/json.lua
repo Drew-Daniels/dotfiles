dbfa9e6fdb67df480f31c5d3fe0cc1f4842d07de
Drew Daniels <daniels_drew@protonmail.com>
2024-01-28 19:18:23 -0600

    rename json snippet

--- nvim/luasnippets/json.lua
+++ nvim/luasnippets/json.lua
@@ -3,5 +3,5 @@
 local helpers = require("luasnip-helpers")
 
 return {
-	s("kv", fmta([[ "<>": <> ]], { i(1), c(2, { sn(nil, { t'"', i(1), t'"' }), t"" } ) }), { desc = "key, value JSON pair" } ),
+	s("jp", fmta([[ "<>": <> ]], { i(1), c(2, { sn(nil, { t'"', i(1), t'"' }), t"" } ) }), { desc = "JSON Property" } ),
 }
