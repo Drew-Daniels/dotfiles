167d66e7db1890f0e7ad9e5ecae2bee73685bc17
Drew Daniels <daniels_drew@protonmail.com>
2024-02-11 17:12:48 -0600

    reconfigure smart-splits, wezterm integration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,45 +2,8 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local config = wezterm.config_builder()
 
--- SMART-SPLITS.NVIM
--- https://github.com/mrjones2014/smart-splits.nvim
-local function is_vim(pane)
-  -- this is set by the plugin, and unset on ExitPre in Neovim
-  return pane:get_user_vars().IS_NVIM == 'true'
-end
-
-local direction_keys = {
-  Left = 'h',
-  Down = 'j',
-  Up = 'k',
-  Right = 'l',
-  -- reverse lookup
-  h = 'Left',
-  j = 'Down',
-  k = 'Up',
-  l = 'Right',
-}
-
-local function split_nav(resize_or_move, key)
-  return {
-    key = key,
-    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
-    action = wezterm.action_callback(function(win, pane)
-      if is_vim(pane) then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
-        }, pane)
-      else
-        if resize_or_move == 'resize' then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
-end
+local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
+-- you can put the rest of your Wezterm config here
 
 -- KEY BINDINGS
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
@@ -99,17 +62,10 @@ config.keys = {
 			mode = "SwapWithActive",
 		}),
 	},
-  -- move between split panes
-  split_nav('move', 'h'),
-  split_nav('move', 'j'),
-  split_nav('move', 'k'),
-  split_nav('move', 'l'),
-  -- resize panes
-  split_nav('resize', 'h'),
-  split_nav('resize', 'j'),
-  split_nav('resize', 'k'),
-  split_nav('resize', 'l'),
 }
+
+-- apply smart_splits defaults
+smart_splits.apply_to_config(config)
 
 -- COLORS
 config.color_scheme = "Gruvbox Material (Gogh)"
