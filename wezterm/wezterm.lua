45950e89a7edebc49949b7fe5984a625436fecd6
Drew Daniels <daniels_drew@protonmail.com>
2024-02-11 15:12:12 -0600

    change wezterm theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,21 +1,29 @@
-local wezterm = require 'wezterm';
-
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.leader = { key = 'a', mods = 'CTRL' , timeout_milliseconds = 1000 }
-
+-- KEY BINDINGS
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  -- splitting
-  {
-    mods   = "LEADER",
-    key    = "_",
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-  },
-  {
-    mods   = "LEADER",
-    key    = "|",
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
-  }
+	-- splitting
+	{
+		mods = "LEADER",
+		key = "_",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
 }
+
+-- COLORS
+config.color_scheme = "Gruvbox Material (Gogh)"
 
 return config
