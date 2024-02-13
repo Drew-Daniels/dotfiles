f96e716fcb8199850c4b36a63c88c68740831c43
Drew Daniels <drew.daniels@webpt.com>
2024-02-13 16:25:30 -0600

    modularize workspace creation functionality

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -114,12 +114,18 @@ wezterm.on("gui-startup", function(cmd)
     pane:send_text("cls\n")
   end
 
-  local function create_workspace(name, dir)
-    local tab, cmd_pane, window = mux.spawn_window({
+  local function create_bare_workspace(name, dir)
+    local tab, cmd_pane, editor_pane, window = mux.spawn_window({
       workspace = name,
       cwd = dir,
       args = args,
     })
+    fishify_pane(cmd_pane)
+    return tab, cmd_pane, editor_pane, window
+  end
+
+  local function create_workspace(name, dir)
+    local tab, cmd_pane, window = create_bare_workspace(name, dir)
     local editor_pane = cmd_pane:split({
       direction = "Top",
       size = 0.6,
@@ -184,7 +190,7 @@ wezterm.on("gui-startup", function(cmd)
   create_workspace("ops tools", project_dir .. "/ops-tools")
   create_workspace("devdocs", project_dir .. "/devDocs")
   create_workspace("keetman", project_dir .. "/keetman")
-  create_workspace("work notes", project_dir .. "/work_notes")
+  create_bare_workspace("work notes", project_dir .. "/work_notes")
 
 	-- We want to startup in the coding workspace
 	mux.set_active_workspace("dotfiles")
