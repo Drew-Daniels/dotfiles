42872a0e26ab72b9864f74c429c522f839fded36
Drew Daniels <daniels_drew@protonmail.com>
2024-02-17 17:18:14 -0600

    format

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -87,21 +87,21 @@ config.keys = {
 	{ key = "p", mods = "LEADER", action = act.SwitchWorkspaceRelative(-1) },
 	{ key = "n", mods = "SHIFT|CTRL", action = wezterm.action.SpawnWindow },
 	{ key = "F9", mods = "ALT", action = wezterm.action.ShowTabNavigator },
-  {
-    key = 'E',
-    mods = 'CTRL|SHIFT',
-    action = act.PromptInputLine {
-      description = 'Enter new name for tab',
-      action = wezterm.action_callback(function(window, pane, line)
-        -- line will be `nil` if they hit escape without entering anything
-        -- An empty string if they just hit enter
-        -- Or the actual line of text they wrote
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end),
-    },
-  },
+	{
+		key = "E",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 -- apply smart_splits defaults
