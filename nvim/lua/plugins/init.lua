776e65c3ee576ab313077c1580a14d50792e131c
Drew Daniels <ddaniels@beanstalkbenefits.com>
2024-03-18 08:21:56 -0500

    add luarocks, upgrade rest.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -130,7 +130,45 @@ return require("lazy").setup({
 	"stevearc/conform.nvim",
 	{ "folke/neodev.nvim", opts = {} },
 	"ray-x/web-tools.nvim",
-	{ "rest-nvim/rest.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+	{
+		"vhyrro/luarocks.nvim",
+		config = function()
+			require("luarocks").setup({})
+		end,
+	},
+	{
+		"rest-nvim/rest.nvim",
+		ft = "http",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			require("rest-nvim").setup({
+				result_split_horizontal = false,
+				result_split_in_place = true,
+				skip_ssl_verification = false,
+				encode_url = true,
+				highlight = {
+					enabled = true,
+					timeout = 150,
+				},
+				result = {
+					show_url = true,
+					show_curl_command = false,
+					show_http_info = true,
+					show_headers = true,
+					formatters = {
+						json = "jq",
+						html = function(body)
+							return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+						end,
+					},
+				},
+				jump_to_request = false,
+				env_file = ".env",
+				custom_dynamic_variables = {},
+				yank_dry_run = true,
+			})
+		end,
+	},
 	"ludovicchabant/vim-gutentags",
 	"RRethy/nvim-treesitter-endwise",
 	"williamboman/mason.nvim",
