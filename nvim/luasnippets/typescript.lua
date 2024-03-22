79f95a0382fd95d8c0363c45e1b3f9fcd63ad660
Drew Daniels <ddaniels@beanstalkbenefits.com>
2024-03-22 16:41:01 -0500

    move js class snippets into typescript snippets file

--- nvim/luasnippets/typescript.lua
+++ nvim/luasnippets/typescript.lua
@@ -36,4 +36,51 @@ return {
 			}
 		)
 	),
+	s(
+		"cd",
+		fmta(
+			[[ 
+        <>class <> {<>}
+      ]],
+			{ c(1, { t(""), t("export ") }), i(2), i(3) },
+			{ desc = "Class Definition" }
+		)
+	),
+	s(
+		"cc",
+		fmta(
+			[[ 
+        constructor(<>) {<>}
+      ]],
+			{ i(1), i(2) },
+			{ desc = "Class Constructor" }
+		)
+	),
+	s(
+		"cm",
+		fmta(
+			[[ 
+        <><><>(<>)<> {<>}
+      ]],
+			{
+				c(1, { t(""), t("static "), t("private ") }),
+				c(2, { t(""), t("async ") }),
+				i(3),
+				i(4),
+				c(5, { t(""), { t(": "), i(1) } }),
+				i(6),
+			},
+			{ desc = "Class Method" }
+		)
+	),
+	s(
+		"cs",
+		fmta(
+			[[ 
+        super(<>)
+      ]],
+			{ i(1) },
+			{ desc = "Class Super" }
+		)
+	),
 }
