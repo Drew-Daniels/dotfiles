f0fe0878ef7d17950bc6f7f90465ce315361fd38
Drew Daniels <daniels_drew@protonmail.com>
2024-03-28 18:14:12 -0500

    update keybindings for codeium

--- nvim/init.lua
+++ nvim/init.lua
@@ -1641,10 +1641,10 @@ require("gitsigns").setup({
 vim.g.codeium_no_map_tab = 1
 -- defaults: https://github.com/Exafunction/codeium.vim?tab=readme-ov-file#%EF%B8%8F-keybindings
 -- set the Meta key in iTerm2 > Preferences > Profiles > Keys > Left Option Key to Esc+
-vim.keymap.set("i", "<M-g>", function()
+vim.keymap.set("i", "<C-g>", function()
 	return vim.fn["codeium#Accept"]()
 end, { expr = true, silent = true })
-vim.keymap.set("i", "<M-x>", function()
+vim.keymap.set("i", "<C-x>", function()
 	return vim.fn["codeium#Clear"]()
 end, { expr = true, silent = true })
 
