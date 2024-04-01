6cf928528d6403d7d4f6f36829c8036bd2fc8a7f
Drew Daniels <daniels_drew@protonmail.com>
2024-04-01 08:22:16 -0500

    deactivate faker

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -16,14 +16,14 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 return require("lazy").setup({
-	{
-		-- development
-		dir = "~/projects/faker.nvim",
-		name = "faker",
-		-- once released
-		-- "Drew-Daniels/faker.nvim",
-		lazy = false,
-	},
+	-- {
+	-- 	-- development
+	-- 	dir = "~/projects/faker.nvim",
+	-- 	name = "faker",
+	-- 	-- once released
+	-- 	-- "Drew-Daniels/faker.nvim",
+	-- 	lazy = false,
+	-- },
 	{
 		"vim-denops/denops.vim",
 	},
