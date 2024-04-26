6ef874b8f09c8405c52eb5fc6bfc9d8da0cb338a
Drew Daniels <ddaniels@beanstalkbenefits.com>
2024-04-26 09:55:02 -0500

    add snippets for adding react comment start and end characters

--- nvim/luasnippets/javascriptreact/comments.lua
+++ nvim/luasnippets/javascriptreact/comments.lua
@@ -5,4 +5,6 @@ local get_visual = helpers.get_visual
 
 return {
 	s("rc", fmta([[{/* <> */}]], { i(1) }), { desc = "React Comment" }),
+	s("rcs", t("{/*"), { desc = "React Comment START" }),
+	s("rce", t("*/}"), { desc = "React Comment END" }),
 }
