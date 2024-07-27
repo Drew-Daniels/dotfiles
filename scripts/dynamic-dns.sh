6bce9e268ffd2472af63b779c995fd0d6aa47ef5
Drew Daniels <daniels_drew@protonmail.com>
2024-07-27 16:33:54 -0500

    add some documentation to dynamic dns script

--- scripts/dynamic-dns.sh
+++ scripts/dynamic-dns.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Get my current Public IP Address
 IP=$(curl -s checkip.amazonaws.com)
+# IP=255.255.255.255
 NAME="$VPN_DOMAIN_NAME."
 TYPE=A
 
@@ -10,13 +12,16 @@ if [[ ! $IP =~ ^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$ ]]; then
 
 fi
 
+# Get the current Public IP Address mapped to my registered domain and save this to a file
 aws route53 list-resource-record-sets --hosted-zone-id Z04722771N10EI4U5OAF0 | jq -r --arg NAME "$NAME" --arg TYPE $TYPE '.ResourceRecordSets[] | select (.Name == $NAME) | select (.Type == $TYPE) | .ResourceRecords[0].Value' >/tmp/current_route53_value
 
+# Check if my current Public IP Address matches the IP Address for my registered domain and if it does - exit
 if grep -Fxq "$IP" /tmp/current_route53_value; then
   echo "IP Has Not Changed, Exiting"
   exit 1
 fi
 
+# Otherwise, update the DNS record in AWS
 cat >/tmp/route53_changes.json <<EOF
 
 {
