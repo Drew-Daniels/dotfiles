264d800b6dcb35d2ab98bccf2ed9f09a1ba24f10
Drew Daniels <daniels_drew@protonmail.com>
2024-07-27 12:56:17 -0500

    add snippet to create shebang line

--- nvim/luasnippets/sh.lua
+++ nvim/luasnippets/sh.lua
@@ -5,4 +5,5 @@ local get_visual = helpers.get_visual
 
 return {
 	s("td", fmta("# TODO: <>", { i(1) }), { desc = "TODO" }),
+  s("sh", fmta("#!/usr/bin/env <>", { i(1, "bash") }), { desc = "Shebang" }),
 }
