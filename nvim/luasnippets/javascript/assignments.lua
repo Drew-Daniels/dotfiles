ce7377ef9405273ed2b1b15f2552b98ed6ae998e
Drew Daniels <daniels_drew@protonmail.com>
2024-08-04 18:22:52 -0500

    update deps

--- nvim/luasnippets/javascript/assignments.lua
+++ nvim/luasnippets/javascript/assignments.lua
@@ -4,6 +4,6 @@ local helpers = require("luasnip-helpers")
 local get_visual = helpers.get_visual
 
 return {
-	s("do", fmta("const {<>} = <>", { i(2), i(1) }), { desc = "Destructure Object" }),
-	s("da", fmta("const [<>] = <>", { i(2), i(1) }), { desc = "Destructure Array" }),
+	s("od", fmta("const {<>} = <>", { i(2), i(1) }), { desc = "Object Destructure" }),
+	s("ad", fmta("const [<>] = <>", { i(2), i(1) }), { desc = "Array Destructure" }),
 }
