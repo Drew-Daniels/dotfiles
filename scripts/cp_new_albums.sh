3ca355adb23526f5714a9950261b0876a9327ba4
Drew Daniels <daniels_drew@protonmail.com>
2024-08-19 21:24:43 -0500

    WIP for copying files over to rpi

--- scripts/cp_new_albums.sh
+++ scripts/cp_new_albums.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# for every folder in the Mac Music directory
-# check if any album in the dir is not in the RPi Music directory
-# if it is not, copy it to the RPi Music directory
 set -e
 
 for dir in "$MAC_MUSIC_FOLDER"/*; do
@@ -10,16 +7,16 @@ for dir in "$MAC_MUSIC_FOLDER"/*; do
     artist_name=$(basename "$dir")
     album_name=$(basename "$subdir")
 
-    escaped_artist_name=$(echo "$artist_name" | sed 's/ /\\ /g')
-    escaped_album_name=$(echo "$album_name" | sed 's/ /\\ /g')
+    escaped_artist_name=$(printf '%q\n' "$artist_name")
+    escaped_album_name=$(printf '%q\n' "$album_name")
 
     cp_from_path="${MAC_MUSIC_FOLDER}/${escaped_artist_name}/${escaped_album_name}"
     cp_to_path="${RPI_MUSIC_FOLDER}/${escaped_artist_name}/${escaped_album_name}"
 
     if ssh "$RPI_USER@$RPI_HOSTNAME" "[ ! -d $cp_to_path ]"; then
       {
-        mkdir -p "$cp_to_path"
-        echo "Copying: $cp_from_path"
+        ssh "$RPI_USER@$RPI_HOSTNAME" "mkdir -p $cp_to_path"
+        scp -r "$cp_from_path" "$RPI_USER@$RPI_HOSTNAME:$cp_to_path"
       }
     fi
   done
