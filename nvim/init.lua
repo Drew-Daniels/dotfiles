d5ebb8aa03d392688322906b4d58c9159cbee268
Drew Daniels <daniels_drew@protonmail.com>
2024-08-21 14:41:34 -0500

    do not autoformat norg files since there is not a formatter to use on these fts

--- nvim/init.lua
+++ nvim/init.lua
@@ -678,9 +678,13 @@ require("lualine").setup({
 --          │        https://github.com/stevearc/conform.nvim         │
 --          ╰─────────────────────────────────────────────────────────╯
 require("conform").setup({
-  format_after_save = {
-    lsp_format = "fallback",
-  },
+  format_after_save = function(bufnr)
+    local ignore_filetypes = { "norg" }
+    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+      return
+    end
+    return { timeout_ms = 500, lsp_format = "fallback" }
+  end,
   formatters_by_ft = {
     c = { "clang-format" },
     lua = { "stylua" },
