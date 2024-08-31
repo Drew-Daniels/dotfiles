64218431d8b689727e37352addf66057a3678728
Drew Daniels <daniels_drew@protonmail.com>
2024-08-31 16:23:35 -0500

    add configuration step for /etc/shells

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,6 +11,9 @@ local_env_file=~/projects/dotfiles/.env.local
 
 test -f $local_env_file || cp $local_env_file_template $local_env_file
 
+# configure /etc/shells
+echo "source ~/projects/dotfiles/etc/shells" >/etc/shells
+
 # configure zsh
 test -f ~/.zshrc || echo ". ~/projects/dotfiles/zsh/.zshrc" >~/.zshrc
 
