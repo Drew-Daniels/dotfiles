effd940a2d634cc3cdd7a7a59fdab69bee4e6131
Drew Daniels <drew.daniels@kipuhealth.com>
2024-09-24 10:08:01 -0500

    extend css snippets into less files

--- nvim/init.lua
+++ nvim/init.lua
@@ -629,6 +629,7 @@ ls.filetype_extend("javascriptreact", { "javascript" })
 ls.filetype_extend("typescript", { "javascript" })
 ls.filetype_extend("vue", { "javascript", "typescript" })
 ls.filetype_extend("typescriptreact", { "javascriptreact", "javascript", "typescript" })
+ls.filetype_extend("less", { "css" })
 ls.filetype_extend("scss", { "css" })
 
 -- Use LspAttach autocommand to only map the following keys
