d41233352b1fcaaba8556e6b7a37e1b996efff95
Drew Daniels <drew.daniels@kipuhealth.com>
2024-09-25 07:49:26 -0500

    add snippet for creating vue templates

--- nvim/luasnippets/vue.lua
+++ nvim/luasnippets/vue.lua
@@ -11,5 +11,6 @@ return {
   s("cp", fmta("const <> = computed(() =>> {<>})", { i(1), i(2) }), { desc = "Comptuted Property" }),
   --TODO: Add newline, tab before interpolation
   --TODO: Add choice node for adding 'scope' directive
+  s("tm", fmt("<template{}>{}</template>", { c(1, { t(""), i(1) }), i(2) }), { desc = "Template" }),
   s("st", fmt("<style{}>{}</style>", { c(1, { t(""), t(" scoped") }), i(2) }), { desc = "Style Tag" }),
 }
