80e1d1ef2b8f93f394cbe6dc14725354eb62f0b6
Drew Daniels <drew.daniels@kipuhealth.com>
2024-09-26 14:09:08 -0500

    temporarily disable format on save with conform

--- nvim/init.lua
+++ nvim/init.lua
@@ -690,13 +690,13 @@ require("lualine").setup({
 --          │        https://github.com/stevearc/conform.nvim         │
 --          ╰─────────────────────────────────────────────────────────╯
 require("conform").setup({
-  format_after_save = function(bufnr)
-    local ignore_filetypes = { "norg" }
-    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-      return
-    end
-    return { timeout_ms = 500, lsp_format = "fallback" }
-  end,
+  -- format_after_save = function(bufnr)
+  --   local ignore_filetypes = { "norg" }
+  --   if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+  --     return
+  --   end
+  --   return { timeout_ms = 500, lsp_format = "fallback" }
+  -- end,
   formatters_by_ft = {
     c = { "clang-format" },
     lua = { "stylua" },
