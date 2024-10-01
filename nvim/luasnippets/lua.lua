504af3bf0173cad1cb39a05ac2458e2a270ce97f
Drew Daniels <drew.daniels@kipuhealth.com>
2024-10-01 08:22:07 -0500

    add a snippet to easily configure nvim plugins

--- nvim/luasnippets/lua.lua
+++ nvim/luasnippets/lua.lua
@@ -5,4 +5,14 @@ return {
   s("td", fmta([[--TODO: <>]], { i(2) }), { desc = "TODO Note" }),
   s("tn", fmta([[t(<>)]], { i(1) }), { desc = "Luasnip Text Node" }),
   s("cn", fmta([[c(<>, { <>, <> })]], { i(1), i(2), i(3) }), { desc = "Luasnip Choice Node" }),
+  s(
+    "su",
+    fmta(
+      [[require("<>").setup({
+  <>
+})]],
+      { i(1), i(2) }
+    ),
+    { desc = "Setup" }
+  ),
 }
