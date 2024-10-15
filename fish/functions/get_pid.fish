13b2e9feb346d0e6ba976db8ab8c60f18556ffdc
Drew Daniels <drew.daniels@kipuhealth.com>
2024-10-15 08:26:54 -0600

    hone in on pid

--- fish/functions/get_pid.fish
+++ fish/functions/get_pid.fish
@@ -1,3 +1,3 @@
 function get_pid --description "Locates process(es) matching the grep pattern"
-    ps -ef | grep "$argv" | grep -v grep
+    ps -ef | grep "$argv" | grep -v grep | awk '{print $2}'
 end
