d77ecbfd8c4d6e45bbbb3271d795f0025c34568b
Drew Daniels <drew.daniels@kipuhealth.com>
2024-10-21 09:11:36 -0500

    disable lsp log

--- nvim/init.lua
+++ nvim/init.lua
@@ -1303,6 +1303,10 @@ set.termguicolors = true
 set.virtualedit = "block"
 set.inccommand = "split"
 
+-- Deactivate LSP logging except only when necessary, since this file can become huge overtime when permanently left on
+-- vim.lsp.set_log_level("debug")
+vim.lsp.set_log_level("off")
+
 -- disable mouse
 set.mouse = ""
 -- ╓
