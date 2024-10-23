7639d1360ccd8520afe47e29e702c863e37c6da2
Drew Daniels <drew.daniels@kipuhealth.com>
2024-10-23 10:00:13 -0500

    fix fn name, add todos

--- fish/functions/fq_hm.fish
+++ fish/functions/fq_hm.fish
@@ -1,4 +1,6 @@
-function fq_rails --description "Forcefully quits rails"
+function fq_hm --description "Forcefully quits rails"
+    # TODO: Check if file exists first before attempting to remove
+    # TODO: Add logging that explains what is happening at each step
     # TODO: De-hardcode path to hm
     rm /Users/drew.daniels/projects/sites/healthmatters/tmp/pids/server.pid
     ps -ef | grep rails | grep -v grep | awk '{print $2}' | xargs kill -9
