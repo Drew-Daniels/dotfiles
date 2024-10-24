9103f5cb3f3f0cc7cc70d3fada2b7137e599e10a
Drew Daniels <daniels_drew@protonmail.com>
2024-10-23 21:24:43 -0500

    Revert "configure image.nvim, and install image displayer backend"
    This reverts commit 1b018003f16378d0dd30d299bad8acfaaf646408.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1250,26 +1250,6 @@ require("scratch").setup({
   filetypes = { "js", "json", "sh", "ruby" },
 })
 
-require("image").setup({
-  backend = "ueberzug",
-  integrations = {
-    markdown = {
-      enabled = true,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-    },
-    neorg = {
-      enabled = true,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      filetypes = { "norg" },
-    },
-  },
-})
-
 -- ── GENERAL ─────────────────────────────────────────────────────────
 local set = vim.opt
 
