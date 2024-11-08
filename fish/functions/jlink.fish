9aa26a389bdae600e7068a7765fe86f1fa2c3674
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-08 10:12:19 -0600

    add -m flag to jlink function

--- fish/functions/jlink.fish
+++ fish/functions/jlink.fish
@@ -1,10 +1,18 @@
 function jlink -d "Copies Jira Issue Link for Current Git Branch"
+    # TODO: add long option
+    argparse m -- $argv
+
     # parse text after first forward slash and before second forward slash and trim whitespace including newlines and put this into a local variable
     set -l jira_issue_id (git branch --show-current | cut -d / -f2- | cut -d / -f1 | tr -d '[:space:]')
 
     set -l jira_issue_link (jira open $jira_issue_id -n | tr -d '\n')
 
-    echo -n $jira_issue_link | pbcopy
-
-    echo "Copied URL to Clipboard: $jira_issue_link"
+    if set -q _flag_m
+        set -l jira_issue_md_link "[$jira_issue_id]($jira_issue_link)"
+        echo $jira_issue_md_link | pbcopy
+        echo "Copied URL to Clipboard: $jira_issue_md_link"
+    else
+        echo $jira_issue_link | pbcopy
+        echo "Copied URL to Clipboard: $jira_issue_link"
+    end
 end
