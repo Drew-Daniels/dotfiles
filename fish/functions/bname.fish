5a83ede15f5224b863133b502d9a9fc82a9f96cd
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-14 11:35:13 -0600

    cleanup

--- fish/functions/bname.fish
+++ fish/functions/bname.fish
@@ -1,26 +1,20 @@
 function bname -d "Generates a Git branch name using a Jira Ticket ID"
     # TODO: Only add "EMR-" prefix if not passed in argument to bname
-    # TODO: Get the scope from whatever comes before the first ":"
-    # TODO: Replace spaces in summary with dashes
-    # TODO: Cut off the summary after the first n characters
-    # get the jira issue id to fetch data against
+    # TODO: Cut off the summary after the first n characters - would be nice to ensure that only whole words are output
     set -l jira_ticket_id $argv[1]
-    # get issue data
-    set -l raw_issue_data (jira issue view $jira_ticket_id --raw)
-    # get issue type (Story, Bug)
-    set -l issue_type (echo $raw_issue_data | jq -r '.fields.issuetype.name')
-    echo $issue_type
 
-    # get issue summary
+    set -l raw_issue_data (jira issue view $jira_ticket_id --raw)
+
+    set -l issue_type (echo $raw_issue_data | jq -r '.fields.issuetype.name')
     set -l issue_scope_and_summary (echo $raw_issue_data | jq -r '.fields.summary')
-    echo $issue_scope_and_summary
+
     set -l issue_scope (echo $issue_scope_and_summary | cut -d ':' -f1 | tr -d '[:space:]')
-    echo $issue_scope
+    set -l issue_summary (echo $issue_scope_and_summary | cut -d ':' -f2 | sed 's/ //' | tr ' ' '-' | tr A-Z a-z)
 
     if test $issue_type = Story
-        echo -n "feat/EMR-$jira_ticket_id/$issue_scope-"
+        echo -n "feat/EMR-$jira_ticket_id/$issue_scope-$issue_summary"
     else
-        echo -n "fix/EMR-$jira_ticket_id/$issue_scope-"
+        echo -n "fix/EMR-$jira_ticket_id/$issue_scope-$issue_summary"
     end
 
 end
