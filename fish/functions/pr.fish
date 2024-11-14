fef3ecb04e34321180f8264010b2c8889fcd26de
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-14 08:19:52 -0600

    set better formatting for gh links

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -1,13 +1,15 @@
 function pr -d "Generates a Slack Message to Link to a Jira Ticket and Pull Request"
     # TODO: Add quiet option
     # TODO: Add option to return result instead of copying to the clipboard
-    # TODO: Improve GH url link formatting to use markdown syntax
     set -l jira_issue_md_link (jlink -m)
     set -l jira_issue_id (jlink -i)
 
-    set -l gh_link (gh search prs $jira_issue_id --assignee="@me" --json=title,url --match=title --limit=1 | jq -r '.[0].url')
+    set -l gh_number_and_link (gh search prs $jira_issue_id --assignee="@me" --json=number,title,url --match=title --limit=1 | jq -r '.[0] | [.number, .url] | join(" ")')
+    set -l gh_number (echo $gh_number_and_link | cut -d ' ' -f1)
+    set -l gh_link (echo $gh_number_and_link | cut -d ' ' -f2)
+    set -l gh_md_link "[#$gh_number]($gh_link)"
 
-    set -l message "PR for $jira_issue_md_link: $gh_link"
+    set -l message "PR for $jira_issue_md_link: $gh_md_link"
 
     echo -n $message | pbcopy
     echo "Copied Slack Message to Clipboard: $message"
