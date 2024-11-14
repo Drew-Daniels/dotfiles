4e1b9f908f93bc128eb38c245035913709f8a2c7
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-14 13:09:31 -0600

    add handling for parsing jira ticket identifiers including emr prefix

--- fish/functions/bname.fish
+++ fish/functions/bname.fish
@@ -1,5 +1,10 @@
 function bname -d "Generates a Git branch name using a Jira Ticket ID"
     # TODO: Add error handling
+    # TODO: Enable getting jira id from all forms:
+    # - EMR-12345 (already covered)
+    # - 12345 (already covered)
+    # - https://kipusystems.atlassian.net/browse/EMR-17631
+    # - https://kipusystems.atlassian.net/browse/EMR-17631?atlOrigin=eyJpIjoiNDg0YzVkMDE0ZTA3NDMzYmEyNmQyMDY3ZjBhNGQ5ZDMiLCJwIjoiaiJ9
     set -l options h/help q/quiet c/clipboard
 
     argparse $options -- $argv
@@ -13,7 +18,6 @@ function bname -d "Generates a Git branch name using a Jira Ticket ID"
         return 0
     end
 
-    # validate
     if test -z "$_flag_c"; and set -q _flag_q
         echo "Cannot use quiet mode without copying to clipboard"
         return 1
@@ -23,6 +27,11 @@ function bname -d "Generates a Git branch name using a Jira Ticket ID"
     # TODO: Only add "EMR-" prefix if not passed in argument to bname
     # TODO: Cut off the summary after the first n characters - would be nice to ensure that only whole words are output
     set -l jira_ticket_id $argv[1]
+
+    if not string match -qi "*emr*" $jira_ticket_id
+        set jira_ticket_id EMR-$jira_ticket_id
+    end
+    echo $jira_ticket_id
 
     set -l raw_issue_data (jira issue view $jira_ticket_id --raw)
 
@@ -44,16 +53,16 @@ function bname -d "Generates a Git branch name using a Jira Ticket ID"
     # TODO: Store shared string in a variable
     if test $issue_type = Story
         if set -q _flag_c
-            echo -n "feat/EMR-$jira_ticket_id/$issue_scope-$issue_summary" | pbcopy
+            echo -n "feat/$jira_ticket_id/$issue_scope-$issue_summary" | pbcopy
             if test -z "$_flag_q"
-                echo "Copied Git Branch Name to Clipboard: feat/EMR-$jira_ticket_id/$issue_scope-$issue_summary"
+                echo "Copied Git Branch Name to Clipboard: feat/$jira_ticket_id/$issue_scope-$issue_summary"
             end
         else
-            echo -n "feat/EMR-$jira_ticket_id/$issue_scope-$issue_summary"
+            echo -n "feat/$jira_ticket_id/$issue_scope-$issue_summary"
         end
     else
         if set -q _flag_c
-            echo -n "fix/EMR-$jira_ticket_id/$issue_scope-$issue_summary" | pbcopy
+            echo -n "fix/$jira_ticket_id/$issue_scope-$issue_summary" | pbcopy
             if test -z "$_flag_q"
                 echo "Copied Git Branch Name to Clipboard: fix/EMR-$jira_ticket_id/$issue_scope-$issue_summary"
             end
