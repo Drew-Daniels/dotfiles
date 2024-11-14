922307b9dde00060497569bb3cf892b98df4e7bf
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-14 08:07:59 -0600

    remove @here, and add todo about formatting gh link

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -1,12 +1,13 @@
 function pr -d "Generates a Slack Message to Link to a Jira Ticket and Pull Request"
     # TODO: Add quiet option
     # TODO: Add option to return result instead of copying to the clipboard
+    # TODO: Improve GH url link formatting to use markdown syntax
     set -l jira_issue_md_link (jlink -m)
     set -l jira_issue_id (jlink -i)
 
     set -l gh_link (gh search prs $jira_issue_id --assignee="@me" --json=title,url --match=title --limit=1 | jq -r '.[0].url')
 
-    set -l message "@here PR for $jira_issue_md_link: $gh_link"
+    set -l message "PR for $jira_issue_md_link: $gh_link"
 
     echo -n $message | pbcopy
     echo "Copied Slack Message to Clipboard: $message"
