ce8e1b006cc4affa06607ac1d57771fe7cb37c66
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-14 06:59:19 -0600

    finish writing 'pr' utility function

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -5,8 +5,7 @@ function pr -d "Generates a Slack Message to Link to a Jira Ticket and Pull Requ
     jlink -i
     set -l jira_issue_id (pbpaste)
 
-    # TODO: Figure out how to interpolate issue id into jq query
-    set -l gh_link (gh search prs --assignee="@me" --json=title,url -q '.[] | select(.title | contains("17631")) | .url')
+    set -l gh_link (gh search prs $jira_issue_id --assignee="@me" --json=title,url --match=title --limit=1 | jq -r '.[0].url')
 
     set -l message "@here PR for $jira_issue_md_link: $gh_link"
 
