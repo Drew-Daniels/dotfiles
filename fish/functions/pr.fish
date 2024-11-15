18caf78384f10ed31bba2bc655588e92a4d2333f
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-15 14:16:31 -0600

    add error handling for when a gh pr cannot be found for a given jira ticket

--- fish/functions/pr.fish
+++ fish/functions/pr.fish
@@ -1,5 +1,4 @@
 function pr -d "Generates a Slack Message to Link to a Jira Ticket and Pull Request"
-    # TODO: Add error handling in the event where there is no matching PR for a ticket
     set -l options q/quiet c/clipboard h/help
     argparse $options -- $argv
 
@@ -15,6 +14,12 @@ function pr -d "Generates a Slack Message to Link to a Jira Ticket and Pull Requ
     set -l jira_issue_id (jlink -i)
 
     set -l gh_number_and_link (gh search prs $jira_issue_id --assignee="@me" --json=number,title,url --match=title --limit=1 | jq -r '.[0] | [.number, .url] | join(" ")')
+
+    if string match -q " " "$gh_number_and_link"
+        echo "No PR found for Jira Issue: $jira_issue_id"
+        return 1
+    end
+
     set -l gh_number (echo $gh_number_and_link | cut -d ' ' -f1)
     set -l gh_link (echo $gh_number_and_link | cut -d ' ' -f2)
     set -l gh_md_link "[#$gh_number]($gh_link)"
