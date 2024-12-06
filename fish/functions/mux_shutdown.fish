942897a7b3da6dd271ca9382acfdfeda0c3e071b
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-06 16:46:54 -0600

    update mux_shutdown to get a list of all active tmuxinator projects

--- fish/functions/mux_shutdown.fish
+++ fish/functions/mux_shutdown.fish
@@ -1,4 +1,7 @@
 function mux_shutdown -d "Gracefully shuts down all tmuxinator projects"
+    # get a list of all active tmuxinator projects
+    # TODO: Create an array from this string and iteratively stop all projects
+    set -l projects (mux list -a | tail -n+2 | sed -r 's/ +/ /g;')
     mux stop dotfiles
     mux stop work_notes
     mux stop healthmatters
