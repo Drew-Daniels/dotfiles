95c9f373c6827e9d7763d80d638a59d9449ff0d0
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-07 20:34:50 -0600

    remove jg.fish

--- fish/conf.d/jg.fish
+++
@@ -1,8 +0,0 @@
-function __jg_install -e jg_install
-    echo (jg -v)" is installed and ready to use!"
-    echo "Just run the `jg` command if you want explore the API."
-end
-
-function __jg_uninstall -e jg_uninstall
-    echo "jg was uninstalled successfully."
-end
