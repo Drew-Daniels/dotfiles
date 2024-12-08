cee9b9950e07cc8e24c9c8528c0428eea501ccd0
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-08 06:54:44 -0600

    update workstation setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,10 +8,10 @@ test -f $local_mise_conf || touch $local_mise_conf
 local_env_file_template=~/projects/dotfiles/.env.template
 local_env_file=~/projects/dotfiles/.env.local
 
-test -f $local_env_file || cp $local_env_file_template $local_env_file
+cp $local_env_file_template $local_env_file
 
 # configure zsh
-test -f ~/.zshrc || echo ". ~/projects/dotfiles/zsh/.zshrc" >~/.zshrc
+echo ". ~/projects/dotfiles/zsh/.zshrc" >~/.zshrc
 
 # configure git
-test -f ~/.gitconfig || cat ~/projects/dotfiles/git/.gitconfig.template >~/.gitconfig
+cat ~/projects/dotfiles/git/.gitconfig.template >~/.gitconfig
