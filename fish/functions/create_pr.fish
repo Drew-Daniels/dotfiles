ab4c5139b1b619fe777c8589442f2ad61abef351
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-10 12:30:46 -0600

    refactor

--- fish/functions/create_pr.fish
+++ fish/functions/create_pr.fish
@@ -1,8 +1,10 @@
 function create_pr -d "Creates a PR"
     set -l title (jg cc)
     echo "Creating PR for $title"
-    # TODO: Use git to get correct base branch (main, encounters-dev, etc) Ex., `git log --first-parent`
-    gh pr create --base encounters-dev --title=$title --assignee=@me --web --draft
+    # gh pr create --base encounters-dev --title=$title --assignee=@me --web --draft --template=$XDG_CONFIG_HOME/projects/dotfiles/kipu_pr_template.md
     # TODO: Use a custom template that attaches related PRs and inserts before & after markdown table 
+    set -l issue_key (jg key)
+    set -l related_prs (related_prs $issue_key)
+    printf $related_prs
     # https://cli.github.com/manual/gh_pr_create
 end
--- fish/functions/related_prs.fish
+++ fish/functions/related_prs.fish
@@ -1,4 +1,16 @@
 function related_prs -d "Finds related pull requests and copies the results to the clipboard"
+    set -l options q/quiet c/clipboard h/help
+    argparse $options -- $argv
+
+    if set --query _flag_help
+        printf "Usage: related_prs <JIRA_TICKET_KEY> [OPTIONS]\n\n"
+        printf "Options:\n"
+        printf "  -c/--clipboard  Copy result to clipboard\n"
+        printf "  -q/--quiet      Don't print anything\n"
+        printf "  -h/--help       Prints help and exits\n"
+        return 0
+    end
+
     set -l pr_urls (gh search prs "$argv" --assignee=@me --json=url | jq -r '.[].url')
 
     if test -z "$pr_urls"
@@ -8,6 +20,13 @@ function related_prs -d "Finds related pull requests and copies the results to t
 
     set -l joined (string join "\n- " $pr_urls)
     set -l joined "- $joined"
-    printf "Copied PR Links to clipboard: \n$joined\n"
-    printf $joined | pbcopy
+
+    if set -q _flag_c
+        printf $joined | pbcopy
+        if test -z "$_flag_q"
+            printf "Copied to Clipboard:\n$joined"
+        end
+    else
+        printf $joined
+    end
 end
