175b3785334f88f9872cbc4285bf8b7767e2aa82
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-10 15:08:57 -0600

    refactor

--- fish/functions/create_pr.fish
+++ fish/functions/create_pr.fish
@@ -1,7 +1,8 @@
 function create_pr -d "Creates a PR"
     set -l title (jg cc)
+    printf "PR Title:\n\t$title\n"
 
-    cat $XDG_CONFIG_HOME/kipu_pr_template.md >./tmp/kipu_pr_body.md
+    bat $XDG_CONFIG_HOME/pr_body_template.md >./tmp/pr_body.md
 
     set -l issue_key (jg key)
     set -l related_prs (related_prs $issue_key)
@@ -10,12 +11,14 @@ function create_pr -d "Creates a PR"
         set -l line_num 8
         for pr in (string split " " $related_prs)
             set -l var (string join '' $line_num i)
-            gsed -i "$var - $pr" ./tmp/kipu_pr_body.md
+            gsed -i "$var - $pr" ./tmp/pr_body.md
             set -l line_num (math $line_num + 1)
         end
     end
 
-    # gh pr create --base encounters-dev --title=$title --assignee=@me --web --draft --template=./tmp/kipu_pr_body.md
+    printf "PR Body:\n"
+    bat ./tmp/pr_body.md
+    # gh pr create --base encounters-dev --title=$title --assignee=@me --web --draft --template=./tmp/pr_body.md
 
-    rm ./tmp/kipu_pr_body.md
+    rm ./tmp/pr_body.md
 end
