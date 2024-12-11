dea0f5393d17f8b21603c4d65f8e4f7c6a72e3e3
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-11 11:13:23 -0600

    create resetdb function

---
+++ fish/functions/resetdb.fish
@@ -0,0 +1,25 @@
+function resetdb -d "Resets local database to a back up from S3, and sets everything up for first time use"
+    set -l user (whoami)
+    set -l dbname $WORK_DB_NAME
+    set -l s3_backup $S3_BACKUP_PATH
+    set -l local_backup $LOCAL_BACKUP_PATH
+
+    backupdb
+
+    dropdb $dbname -f
+
+    createdb $dbname -E UTF8 -l en_US.UTF-8 -h localhost -U $user -w
+
+    aws s3 cp $s3_backup $local_backup
+
+    pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER -d $dbname -j 8 $local_backup
+
+    bundle exec rake db:migrate
+
+    bundle exec rake dev:delete_db_encryption
+
+    bundle exec rails runner $XDG_CONFIG_HOME/scripts/work/create_super_admin.rb
+
+    bundle exec rails runner $XDG_CONFIG_HOME/scripts/work/create_doctor.rb
+
+end
