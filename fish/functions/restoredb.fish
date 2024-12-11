f08c3a5340c4a3ca28ac33efcd14a9d4a895fafa
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-11 10:55:16 -0600

    add logging to restoredb function

--- fish/functions/restoredb.fish
+++ fish/functions/restoredb.fish
@@ -6,13 +6,19 @@ function restoredb -d "Restores database for current branch from backup"
 
     if test -f $db_backup_path
         echo "Dropping $db_name"
-        # dropdb $db_name -f
+        dropdb $db_name -f
 
         echo "Creating $db_name database"
-        # createdb -U $user $db_name
+        createdb -U $user $db_name
 
         echo "Restoring $db_name database from $db_backup_path"
-        # psql -U $user -d $db_name -f $db_backup_path
+        psql -U $user -d $db_name -f $db_backup_path
+
+        if test $status -eq 0
+            echo "Database ($db_name) for $current_branch restored"
+        else
+            echo "Database ($db_name) for $current_branch restore failed"
+        end
     else
         echo "Backup for $current_branch does not exist"
     end
