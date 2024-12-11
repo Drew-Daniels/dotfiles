83abf2454f6460d21437cc1e64f3303207d52170
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-11 16:32:55 -0600

    refactor db dump naming logic

--- fish/functions/backupdb.fish
+++ fish/functions/backupdb.fish
@@ -1,8 +1,9 @@
 function backupdb -d "Backs up the database used for current branch"
     set -l user (whoami)
     set -l db_name $WORK_DB_NAME
-    set -l current_branch $(git branch --show-current)
-    set -l db_backup_path $BACKUPS_DIR/$current_branch.dump
+    set -l current_branch (git branch --show-current)
+    set -l dump_file_name (string split -r -m 1 "/" $current_branch | tail -n 1)
+    set -l db_backup_path $BACKUPS_DIR/$dump_file_name.dump
 
     echo "Backing up $current_branch database ($db_name) to $db_backup_path"
     pg_dump -U $user -d $db_name -f $db_backup_path -v --format=custom
--- fish/functions/restoredb.fish
+++ fish/functions/restoredb.fish
@@ -2,7 +2,8 @@ function restoredb -d "Restores database for current branch from backup"
     set -l user (whoami)
     set -l db_name $WORK_DB_NAME
     set -l current_branch $(git branch --show-current)
-    set -l db_backup_path $BACKUPS_DIR/$current_branch.dump
+    set -l dump_file_name (string split -r -m 1 "/" $current_branch | tail -n 1)
+    set -l db_backup_path $BACKUPS_DIR/$dump_file_name.dump
     set -l jobs 14
 
     if test -f $db_backup_path
@@ -13,7 +14,7 @@ function restoredb -d "Restores database for current branch from backup"
         createdb -U $user $db_name
 
         echo "Restoring $db_name database from $db_backup_path"
-        # psql -U $user -d $db_name -f $db_backup_path
+
         pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $user -d $db_name -j $jobs $db_backup_path
 
         if test $status -eq 0
