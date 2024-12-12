80a804939f073695783961c11224c84e4d0d6d41
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-12 14:54:07 -0600

    add handling to make sure I'm on the right branch

--- fish/functions/add_reviewers.fish
+++ fish/functions/add_reviewers.fish
@@ -1,6 +1,13 @@
 function add_reviewers -d "Add reviewers to a pull request"
+    set -l current_branch (git branch --show-current)
+
     if test -z $REVIEWERS
         echo "No reviewers provided"
+        return 1
+    end
+
+    if [ $current_branch = main ] || [ $current_branch = encounters-dev ]
+        echo "Check out a feature branch"
         return 1
     end
 
