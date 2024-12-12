c8ff06010009afa12e0cd39744125ae647f2a43e
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-12 14:27:00 -0600

    wip

--- fish/functions/create_pr.fish
+++ fish/functions/create_pr.fish
@@ -1,23 +1,22 @@
 function create_pr -d "Creates a PR"
     set -l title (jg cc)
-    set -l jira_ticket_md_link (jg url -m)
+    # TODO: Figure out why sed is not replacing the match with the spaces before the -
+    set -l jira_ticket_md_link (jg url -m | sed -E 's/^/    - /')
     set -l tmp_file $PWD/tmp/pr_body.md
     printf "PR Title:\n\t$title\n"
 
     cat $XDG_CONFIG_HOME/work/pr_body_template.md >$tmp_file
 
     set -l issue_key (jg key)
+
+    # Insert Jira ticket link
+    gsed -i "6 a $jira_ticket_md_link" $tmp_file
+
+    # Insert links to related PRs
     # TODO: Modify to use 'jg related' at some point
+    # TODO: Format these as markdown links
     set -l related_prs (_related_prs $issue_key)
-
-    # TODO: Modify to include link to the Jira ticket in the body
-
     if test $status -eq 0
-        # Append jira ticket link
-        gsed -i "/Link/a $jira_ticket_md_link" $tmp_file
-
-        # TODO: Format these as markdown links
-        # Insert links to related PRs
         set -l line_num 8
         for pr in (string split " " $related_prs)
             set -l var (string join '' $line_num i)
@@ -28,9 +27,10 @@ function create_pr -d "Creates a PR"
 
     printf "PR Body:\n"
     bat $tmp_file --paging=never
-    # cat $tmp_file
 
     # gh pr create --base main --title=$title --assignee=@me --draft --body-file=$tmp_file
+    #
+    # gh pr view --web
 
-    # rm $tmp_file
+    rm $tmp_file
 end
