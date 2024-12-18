b0f65b3a496e42146bb54f207d9cfb6c5f6fc8b5
Drew Daniels <drew.daniels@kipuhealth.com>
2024-12-18 10:02:18 -0600

    deployment utilities

--- fish/functions/main_release_requested.fish
+++ fish/functions/main_release_requested.fish
@@ -1,21 +1,21 @@
-function encounters_release_requested -d "Generates a Slack message when a release is requested"
-    set -l encounters_release_requested_template $XDG_CONFIG_HOME/work/encounters_release_requested_template.md
-    set -l tmp_file $PWD/tmp/encounters_release_requested_template.md
+function main_release_requested -d "Generates a Slack message when a release is requested"
+    set -l main_release_requested_template $XDG_CONFIG_HOME/work/main_release_requested_template.md
+    set -l tmp_file $PWD/tmp/main_release_requested_template.md
 
     # Get list of issues that will be deployed in the next release
-    set -l issue_keys (jira issue list --status="Dev Complete" --plain -q="pod = 'Encounters'" --component=Encounters --columns=key --no-headers | tr '\n' ',' | sed 's/,$//g;')
-
+    set -l issue_keys (jira issue list --status="Dev Complete" --plain -q="pod = 'Core'" --columns=key --no-headers | tr '\n' ',' | sed 's/,$//g;')
     if [ -z $issue_keys ]
         echo "No issues are in Dev Complete. Aborting."
         return 1
     end
 
     # Create temp file from template
-    cat $encounters_release_requested_template >$tmp_file
+    cat $main_release_requested_template >$tmp_file
 
     # Ping /heartbeat to get last release information
-    set -l heartbeat (curl $ENCOUNTERS_DEV_HEARTBEAT_URL)
-    set -l heartbeat_data (string match -r '(.*)\.encounters-dev\.(\d\d\d\d-\d\d-\d\d)\.(\d)\.aws.*' $heartbeat | tail -n3 | tr '\n' ' ')
+    set -l heartbeat (curl $MAIN_HEARTBEAT_URL)
+    # TODO: Verify that 'core-sr' bit is consistent across deployments
+    set -l heartbeat_data (string match -r '(.*)\.core-sr\.(\d\d\d\d-\d\d-\d\d)\.(\d)\.aws.*' $heartbeat | tail -n3 | tr '\n' ' ')
     set -l heartbeat_prefix (echo $heartbeat_data | cut -d ' ' -f 1)
     set -l heartbeat_date (echo $heartbeat_data | cut -d ' ' -f2)
     set -l heartbeat_version (echo $heartbeat_data | cut -d ' ' -f3-)
