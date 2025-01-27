dd98ed0ac21b456faa3bba14de0e6a1035643ca1
Drew Daniels <drew.daniels@kipuhealth.com>
2025-01-27 10:53:52 -0600

    refactor copydb, restoredb scripts

--- fish/functions/copydb.fish
+++ fish/functions/copydb.fish
@@ -9,25 +9,24 @@ function copydb -d "Copies the database from another backup"
     set -l user (whoami)
     set -l current_branch $(git branch --show-current)
     set -l branch_data (string split / $current_branch)
-    set -l db_name $branch_data[2] || set -l db_name $branch_data[1]
-    set -l db_backup_path ~/backups/$source_db_name.dump
+    set -l source_db_backup_path ~/backups/$source_db_name.dump
+    set -l target_db_backup_path ~/backups/$target_db_name.dump
     set -l jobs 14
 
-    if test -f $db_backup_path
-        echo "Dropping $db_name"
-        dropdb $db_name -f
+    if test -f $source_db_backup_path
+        echo "Copying source backup from $source_db_backup_path to $target_db_backup_path"
+        cp $source_db_backup_path $target_db_backup_path
 
-        echo "Creating $db_name database"
+        echo "Creating $target_db_name database"
         createdb -U $user $target_db_name
 
-        echo "Restoring $db_name database from $db_backup_path"
-
-        pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $user -d $target_db_name -j $jobs $db_backup_path
+        echo "Restoring $target_db_name database from $target_db_backup_path"
+        pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $user -d $target_db_name -j $jobs $target_db_backup_path
 
         if test $status -eq 0
-            echo "Database ($db_name) for $current_branch restored"
+            echo "Database ($target_db_name) for $current_branch restored"
         else
-            echo "Database ($db_name) for $current_branch restore failed"
+            echo "Database ($target_db_name) for $current_branch restore failed"
         end
     else
         echo "Backup for $current_branch does not exist"
