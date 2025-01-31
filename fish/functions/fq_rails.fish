12ab79f3d229d2241024bc7318fc5a764cbe654e
Drew Daniels <drew.daniels@kipuhealth.com>
2025-01-31 08:59:43 -0600

    delete and rename

--- fish/functions/fq_rails.fish
+++
@@ -1,5 +0,0 @@
-function fq_rails --description "Forcefully quits rails"
-    # TODO: De-hardcode path to hm
-    rm /Users/drew.daniels/projects/sites/healthmatters/tmp/pids/server.pid
-    ps -ef | grep "$argv" | grep -v grep | awk '{print $2}' | xargs kill -9
-end
