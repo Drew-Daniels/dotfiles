9b49197a664928c164320d0fd9d571c4defcee27
Drew Daniels <daniels_drew@protonmail.com>
2025-02-16 13:56:28 -0600

    clean up which-key config

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -1,13 +1,6 @@
 return {
 	"folke/which-key.nvim",
-	dependencies = {
-		{ "echasnovski/mini.icons", version = false },
-	},
 	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
 	config = function(_, opts)
 		local wk = require("which-key")
 		wk.setup(opts)
