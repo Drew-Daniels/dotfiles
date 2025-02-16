8ca9c08b1ba5f2e31389008279c040021606be78
Drew Daniels <daniels_drew@protonmail.com>
2025-02-16 11:00:53 -0600

    move general configuration to lazy config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -718,43 +718,11 @@ end, { nargs = 1 })
 --
 
 -- ── GENERAL ─────────────────────────────────────────────────────────
-local set = vim.opt
-
--- autoindent new lines
-set.autoindent = true
--- expands tabs into spaces
-set.expandtab = true
--- number of spaces to use for each tab
-set.tabstop = 2
--- number of spaces to use when indenting
-set.shiftwidth = 2
--- number of spaces to use for (auto)indent step
-set.softtabstop = 2
-set.ignorecase = true
-set.smartcase = true
-set.number = true
-set.splitbelow = true
-set.splitright = true
--- set.scrolloff = 999
-set.hlsearch = false
-set.wildignore = "node_modules/*"
-set.number = true
-set.relativenumber = true
-vim.cmd([[autocmd FileType * set formatoptions-=ro]])
-set.syntax = "on"
-set.virtualedit = "block"
-set.inccommand = "split"
 
 -- Deactivate LSP logging except only when necessary, since this file can become huge overtime when permanently left on
 -- vim.lsp.set_log_level("debug")
 vim.lsp.set_log_level("off")
 
--- disable mouse
-set.mouse = ""
--- ╓
--- ║ https://stackoverflow.com/questions/4642822/how-to-make-bashrc-aliases-available-within-a-vim-shell-command
--- ╙
-set.shellcmdflag = "-ic"
 vim.keymap.set("n", "n", "nzz", { silent = true, desc = "Search Next" })
 vim.keymap.set("n", "N", "Nzz", { silent = true, desc = "Search Prev" })
 vim.keymap.set("i", "<C-b>", "<CR><ESC>kA<CR>", { silent = true, desc = "Insert blank line" })
--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -20,7 +20,40 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.opt.termguicolors = true
+local set = vim.opt
+
+set.termguicolors = true
+-- autoindent new lines
+set.autoindent = true
+-- expands tabs into spaces
+set.expandtab = true
+-- number of spaces to use for each tab
+set.tabstop = 2
+-- number of spaces to use when indenting
+set.shiftwidth = 2
+-- number of spaces to use for (auto)indent step
+set.softtabstop = 2
+set.ignorecase = true
+set.smartcase = true
+set.number = true
+set.splitbelow = true
+set.splitright = true
+-- set.scrolloff = 999
+set.hlsearch = false
+set.wildignore = "node_modules/*"
+set.number = true
+set.relativenumber = true
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
+set.syntax = "on"
+set.virtualedit = "block"
+set.inccommand = "split"
+
+-- disable mouse
+set.mouse = ""
+-- ╓
+-- ║ https://stackoverflow.com/questions/4642822/how-to-make-bashrc-aliases-available-within-a-vim-shell-command
+-- ╙
+set.shellcmdflag = "-ic"
 
 -- TODO: Update config to point to custom plugin dir ~/projects
 -- Setup lazy.nvim
