d5379dffcd89c7affbab4398e7ca95854c0c969c
Drew Daniels <drew.daniels@kipuhealth.com>
2025-02-26 14:13:29 -0600

    update backupdb function to use directory format instead of custom compression format

--- dot_config/fish/functions/backupdb.fish
+++ dot_config/fish/functions/backupdb.fish
@@ -4,10 +4,11 @@ function backupdb -d "Backs up the database used for current branch"
     set -l branch_data (string split / $current_branch)
     set -l db_name $branch_data[2] || set -l db_name $branch_data[1]
     set -l full_db_name kipu_demo_development_$db_name
-    set -l db_backup_path ~/backups/$full_db_name.dump
+    set -l db_backup_path ~/backups/$full_db_name
+    set -l jobs (nproc)
 
     echo "Backing up $current_branch database ($full_db_name) to $db_backup_path"
-    pg_dump -U $user -d $full_db_name -f $db_backup_path -v --format=custom
+    pg_dump -U $user -d $full_db_name -f $db_backup_path -v --format=d --jobs=$jobs
 
     if test $status -eq 0
         echo "Successfully backed up $current_branch database to $db_backup_path"
