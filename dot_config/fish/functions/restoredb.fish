c1e83f4800861e157d41339512facc5c083e1ccf
Drew Daniels <drew.daniels@kipuhealth.com>
2025-02-26 14:15:06 -0600

    use nproc to get number of cores

--- dot_config/fish/functions/restoredb.fish
+++ dot_config/fish/functions/restoredb.fish
@@ -5,7 +5,7 @@ function restoredb -d "Restores database for current branch from backup"
     set -l db_name $branch_data[2] || set -l db_name $branch_data[1]
     set -l full_db_name kipu_demo_development_$db_name
     set -l db_backup_path ~/backups/$full_db_name.dump
-    set -l jobs 14
+    set -l jobs (nproc)
 
     if test -f $db_backup_path
         echo "Dropping $full_db_name"
