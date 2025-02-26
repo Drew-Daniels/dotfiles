1a5f74cb3eedb6c48caa1af61b96130cc0f0416e
Drew Daniels <drew.daniels@kipuhealth.com>
2025-02-26 13:01:38 -0600

    implement renamedb utility function

--- dot_config/fish/functions/renamedb.fish
+++ dot_config/fish/functions/renamedb.fish
@@ -1,10 +1,9 @@
 function renamedb -d "renames a postgres database"
-    set args $argv
     set old_name $argv[1]
     set new_name $argv[2]
 
     echo "old_name: $old_name"
     echo "new_name: $new_name"
 
-    # psql -U postgres -c 'ALTER DATABASE "$old_name" RENAME TO "$new_name";'
+    psql -U postgres -c "ALTER DATABASE \"$old_name\" RENAME TO \"$new_name\";"
 end
