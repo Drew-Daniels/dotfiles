41fc5ddc49db77bac80d3e351035e3ee11027bc8
Drew Daniels <drew.daniels@kipuhealth.com>
2025-02-26 14:30:17 -0600

    refactor to use directory format

--- dot_config/fish/functions/copydb.fish
+++ dot_config/fish/functions/copydb.fish
@@ -13,9 +13,12 @@ function copydb -d "Copies the database from another backup"
     set -l target_db_backup_path ~/backups/$target_db_name
     set -l jobs (nproc)
 
-    if test -f $source_db_backup_path
+    echo $source_db_backup_path
+    echo $target_db_backup_path
+
+    if test -d $source_db_backup_path
         echo "Copying source backup from $source_db_backup_path to $target_db_backup_path"
-        cp $source_db_backup_path $target_db_backup_path
+        cp -R $source_db_backup_path $target_db_backup_path
 
         echo "Creating $target_db_name database"
         createdb -U $user $target_db_name
