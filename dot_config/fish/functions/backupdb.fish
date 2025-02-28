d372205bc56d0ef878fabc6624cfbd85350e2eda
Drew Daniels <drew.daniels@kipuhealth.com>
2025-02-28 10:14:19 -0600

    update neovim deps, add step to backupdb script to remove previous backup

--- dot_config/fish/functions/backupdb.fish
+++ dot_config/fish/functions/backupdb.fish
@@ -7,6 +7,9 @@ function backupdb -d "Backs up the database used for current branch"
     set -l db_backup_path ~/backups/$full_db_name
     set -l jobs (nproc)
 
+    echo "Deleting previous backup"
+    rm -rf $db_backup_path
+
     echo "Backing up $current_branch database ($full_db_name) to $db_backup_path"
     pg_dump -U $user -d $full_db_name -f $db_backup_path -v --format=d --jobs=$jobs
 
