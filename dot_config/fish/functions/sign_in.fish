bbd6949f0f856f6a5fe5fedcdfa91833e719db3c
Drew Daniels <drew.daniels@kipuhealth.com>
2025-03-06 18:06:11 -0600

    create helper functions to sign in and sign out of application

---
+++ dot_config/fish/functions/sign_in.fish
@@ -0,0 +1,21 @@
+function sign_in -d "Signs in with curl and saves cookie"
+    # get csrf token
+    # TODO: Rename?
+    set csrf_token (command curl -s localhost:3000/users/sign_in | rg "csrf-token" | sed -En 's/.*content="(.*)".*/\1/p')
+    # sign in with credentials and csrf token
+    # TODO: De-hardcode cookie path
+    # TODO: Return error code when missing env vars
+    # TODO: Use 1Password CLI to securely fetch credentials instead of using env vars
+    echo "Signing in..."
+    command curl -s -o /dev/null -X POST http://localhost:3000/users/sign_in \
+        -F "user[login]=$KIPU_CAS_USER_LOGIN" \
+        -F "user[password]=$KIPU_CAS_USER_PASSWORD" \
+        -F "authenticity_token=$csrf_token" \
+        -c ~/projects/sites/healthmatters/tmp/cookie.txt
+    if test $status -eq 0
+        echo "Signed in"
+    else
+        echo "Unable to sign in"
+        exit 1
+    end
+end
---
+++ dot_config/fish/functions/sign_out.fish
@@ -0,0 +1,10 @@
+function sign_out -d "Signs out"
+    echo "Signing out..."
+    command curl -s -o /dev/null -X POST localhost:3000/users/sign_out
+    if test $status -eq 0
+        echo "Successfully signed out"
+    else
+        echo "An error occurred while trying to sign out"
+        exit 1
+    end
+end
