7275529227026bfc649fbfc62f4856a20763f27d
Drew Daniels <drew.daniels@kipuhealth.com>
2025-03-27 08:55:43 -0500

    fix issue where 'Format' command is not set

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -2,6 +2,19 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
+	init = function()
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_fallback = true, range = range })
+		end, { range = true })
+	end,
 	config = function()
 		local conform_utils = require("conform.util")
 
@@ -164,17 +177,5 @@ return {
 		}
 
 		require("conform").setup(opts)
-
-		vim.api.nvim_create_user_command("Format", function(args)
-			local range = nil
-			if args.count ~= -1 then
-				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-				range = {
-					start = { args.line1, 0 },
-					["end"] = { args.line2, end_line:len() },
-				}
-			end
-			require("conform").format({ async = true, lsp_fallback = true, range = range })
-		end, { range = true })
 	end,
 }
