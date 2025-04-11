a5929e95d711ee744765f6135bc4dbb545179085
Drew Daniels <drew.daniels@webpt.com>
2025-04-11 16:06:41 -0500

    add step to install jg

--- macos.sh
+++ macos.sh
@@ -11,6 +11,10 @@ if [ ! -d ~/projects/work_notes ]; then
   git clone https://github.com/Drew-Daniels/work_notes.git ~/projects/work_notes
 fi
 
+if [ ! -d ~/projects/jg ]; then
+  git clone https://codeberg.org/drewdaniels/jg.git ~/projects/jg
+fi
+
 #          ╭──────────────────────────────────────────────────────────╮
 #          │                         homebrew                         │
 #          │                     https://brew.sh/                     │
