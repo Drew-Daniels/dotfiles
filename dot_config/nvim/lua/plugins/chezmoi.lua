b61c044995c8a1083e9a213d0b9ac8a95d7a1c23
Drew Daniels <daniels_drew@protonmail.com>
2025-04-14 21:23:04 -0500

    migrate to more declarative homebrew approach

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -6,11 +6,11 @@ return {
 		vim.g["chezmoi#use_tmp_buffer"] = true
 		-- add other options here if needed.
 		-- MY OPTIONS START --
-		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-			pattern = "*.sh.tmpl",
-			command = "set filetype=bash",
-			group = "filetypedetect",
-		})
+		-- vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+		-- 	pattern = "*.sh.tmpl",
+		-- 	command = "set filetype=bash",
+		-- 	group = "filetypedetect",
+		-- })
 		-- MY OPTIONS END --
 	end,
 }
