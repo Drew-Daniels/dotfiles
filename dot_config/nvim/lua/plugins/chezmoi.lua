36abaaa4ab364584c68a95272cd11c98f2e1e0d6
Drew Daniels <daniels_drew@protonmail.com>
2025-04-14 19:21:02 -0500

    add setting to recognize '*.sh.tmpl' files as bash files so chezmoi.vim adds syntax highlighting

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,9 +1,16 @@
 return {
-  "alker0/chezmoi.vim",
-  lazy = false,
-  init = function()
-    -- This option is required.
-    vim.g["chezmoi#use_tmp_buffer"] = true
-    -- add other options here if needed.
-  end,
+	"alker0/chezmoi.vim",
+	lazy = false,
+	init = function()
+		-- This option is required.
+		vim.g["chezmoi#use_tmp_buffer"] = true
+		-- add other options here if needed.
+		-- MY OPTIONS START --
+		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+			pattern = "*.sh.tmpl",
+			command = "set filetype=bash",
+			group = "filetypedetect",
+		})
+		-- MY OPTIONS END --
+	end,
 }
