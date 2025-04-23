f0df056d1dd9df0d0aea4f89c311514abe67db78
Drew Daniels <drew.daniels@webpt.com>
2025-04-23 10:22:40 -0500

    markdown-preview.nvim fork

--- dot_config/nvim/lua/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -1,7 +1,8 @@
 return {
-	"iamcco/markdown-preview.nvim",
+	-- forked because of recurring build issues when running ':Lazy sync'
+	"Drew-Daniels/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
+	build = "cd app && yarn install",
 	init = function()
 		vim.g.mkdp_filetypes = { "markdown" }
 	end,
