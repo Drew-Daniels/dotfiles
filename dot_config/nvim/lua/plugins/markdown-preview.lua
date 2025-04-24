a21ab0130e8d450493e0f6873b24dbc8f126dc65
Drew Daniels <drew.daniels@webpt.com>
2025-04-24 08:46:34 -0500

    markdown-preview

--- dot_config/nvim/lua/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,7 @@
 return {
+	"iamcco/markdown-preview.nvim",
 	-- forked because of recurring build issues when running ':Lazy sync'
-	"Drew-Daniels/markdown-preview.nvim",
+	-- "Drew-Daniels/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	build = "cd app && yarn install",
 	init = function()
