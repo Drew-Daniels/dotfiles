4f8bd5989774de4e86bdbf10d97ae46c4a793f3d
Drew Daniels <daniels_drew@protonmail.com>
2025-05-09 17:36:02 -0500

    add signal desktop installation step

--- debian-install.sh
+++ debian-install.sh
@@ -1103,10 +1103,24 @@ else
   echo "Already installed zulip"
 fi
 
-# TODO: Only reboot if something in the system environment has changed as a result of code run in this file. Might be hard to determine/track this. Might be able to use a local variable to track when a change is made, that is worth rebooting for.
+#          ╭──────────────────────────────────────────────────────────╮
+#          │                          signal                          │
+#          │            https://signal.org/download/linux/            │
+#          ╰──────────────────────────────────────────────────────────╯
+# 1. Install our official public software signing key:
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor >signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
+
+# 2. Add our repository to your list of repositories:
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install Signal:
+sudo apt update && sudo apt install signal-desktop
 
 # cleanup
 sudo apt autoremove -y
 sudo apt autoclean -y
 
+# TODO: Only reboot if something in the system environment has changed as a result of code run in this file. Might be hard to determine/track this. Might be able to use a local variable to track when a change is made, that is worth rebooting for.
 # reboot
