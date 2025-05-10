b6f68e33496ebc6366b88988e8ae4d723c43ee19
Drew Daniels <daniels_drew@protonmail.com>
2025-05-09 19:39:13 -0500

    add step to upgrade awscli

--- debian-upgrade.sh
+++ debian-upgrade.sh
@@ -89,21 +89,20 @@ fi
 # TODO: Uncomment once done hashing out the rest
 # sudo apt update -y
 
-# Updates packages that have to be manually updated outside of 'apt'
-# TODO: Suppress curl output
-# TODO: Perform installation steps in a /tmp folder
 #╭──────────────────────────────────────────────────────────────────────────────╮
 #│                                    awscli                                    │
 #│https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html │
 #╰──────────────────────────────────────────────────────────────────────────────╯
-# TODO: Add check to see if there is a later version available, and only install if it is differerent version from current install
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip awscliv2.zip
-# ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
-#
-# # cleanup
-# rm awscliv2.zip
-# rm -rf aws
+# TODO: Figure out how to check for latest available version before attempting to install
+echo "Upgrading awscli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
+# cleanup
+rm awscliv2.zip
+rm -rf aws
+echo "Upgraded awscli"
 
 #          ╭──────────────────────────────────────────────────────────╮
 #          │                          rustup                          │
