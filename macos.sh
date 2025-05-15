3c3ef314f51007d04bc06ed66cb11c6f0d0555e1
Drew Daniels <drew.daniels@webpt.com>
2025-05-15 11:47:19 -0500

    Revert "refactor scripts logic"
    This reverts commit 28fc12c896d0f7dde9797716ba0b6951155da188.

--- macos.sh
+++ macos.sh
@@ -1,4 +1,20 @@
 #!/bin/zsh
+
+mkdir -p ~/projects
+mkdir -p /usr/local/bin
+
+if [ ! -d ~/projects/friendly-snippets ]; then
+  git clone https://github.com/Drew-Daniels/friendly-snippets.git ~/projects/friendly-snippets
+fi
+
+if [ ! -d ~/projects/work_notes ]; then
+  git clone https://github.com/Drew-Daniels/work_notes.git ~/projects/work_notes
+fi
+
+if [ ! -d ~/projects/jg ]; then
+  git clone https://codeberg.org/drewdaniels/jg.git ~/projects/jg
+fi
+
 #          ╭──────────────────────────────────────────────────────────╮
 #          │                         homebrew                         │
 #          │                     https://brew.sh/                     │
--- run_once_00-install-directories.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/projects
-mkdir -p /usr/local/bin
