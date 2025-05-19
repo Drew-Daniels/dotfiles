121335207cee72fc2010c20f58671e0140543cf8
Drew Daniels <daniels_drew@protonmail.com>
2025-05-19 16:42:49 -0500

    track nixos configuration using chezmoi

---
+++ dot_config/nixos/configuration.nix
@@ -0,0 +1,200 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = false;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  security.rtkit.enable = true;
+  services.avahi.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+    raopOpenFirewall = true;
+    extraConfig.pipewire = {
+	    "10-airplay" = {
+		"context.modules" = [
+			{
+				name = "libpipewire-module-raop-discover";
+			}
+		];
+	    };
+    };
+  };
+  
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.drew = {
+    isNormalUser = true;
+    description = "Drew Daniels";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+  # Virtualisation
+  virtualisation.docker = {
+	enable = true;
+	rootless = {
+		enable = true;
+		setSocketVariable = true;
+	};
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Enable sway
+  programs.sway.enable = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+	enable = true;
+	polkitPolicyOwners = ["drew"];
+  };
+
+  # Allow unfree packagesA
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  # wget
+  ];
+
+  # EXPERIMENTAL SETTINGS
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  services.mullvad-vpn.enable = true;
+
+  # VPN
+  # https://alberand.com/nixos-wireguard-vpn.html
+  networking.firewall.allowedUDPPorts = [ 51820 ];
+
+  # To start:
+  # sudo systemctl stop wg-quick-wg0
+
+  # To stop:
+  # sudo systemctl start wg-quick-wg0
+
+  # Debugging Note: If I notice that the wg0 connection no longer exists for some reason,
+  # power off laptop (don't restart), and then manually power back on. After logging back in, rebuild and see if the connection reappears.
+  networking.wg-quick.interfaces = {
+    wg0 = {
+	address = [ "10.0.0.5/24" ];
+	listenPort = 51820;
+	privateKeyFile = "/etc/wireguard/wg0-private-key";
+	peers = [{
+		publicKey = "QiA3AA8yovuB4+vEmU7XmuX05lDZfM2B7njpfZZ4XUg=";
+		allowedIPs = [ "0.0.0.0/0" ];
+		endpoint = builtins.getEnv "HOME_DDNS_NAME";
+		persistentKeepalive = 25;
+	}];
+	autostart = false;
+	dns = [ "64.6.64.6" "10.0.0.1"];
+    };
+  };
+
+  system.autoUpgrade = {
+	enable = true;
+	dates = "02:00";
+  };
+
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
