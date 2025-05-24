be75fafcd6ea30c5139400248f38d0e824d874ab
Drew Daniels <daniels_drew@protonmail.com>
2025-05-24 10:40:16 -0500

    cleanup

--- dot_config/nixos/flake.nix
+++ dot_config/nixos/flake.nix
@@ -1,18 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "NixOS Configuration Flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # TODO: Not sure if this is necessary
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = { 
-      # 'nixos' is the hostname of this machine
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
