614c1b0713b758ad0248851e61c25ecddaa03c70
Drew Daniels <daniels_drew@protonmail.com>
2025-05-25 11:39:45 -0500

    create autocmd to automatically apply changes made to source dir to target dir

--- dot_config/nvim/lua/plugins/chezmoi-nvim.lua
+++ dot_config/nvim/lua/plugins/chezmoi-nvim.lua
@@ -17,4 +17,16 @@ return {
 
 		vim.api.nvim_command("command! ChezmoiFzf lua fzf_chezmoi()")
 	end,
+	init = function()
+		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+			pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+			callback = function(ev)
+				local bufnr = ev.buf
+				local edit_watch = function()
+					require("chezmoi.commands.__edit").watch(bufnr)
+				end
+				vim.schedule(edit_watch)
+			end,
+		})
+	end,
 }
