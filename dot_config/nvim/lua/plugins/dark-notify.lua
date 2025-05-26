c9d837d8a50844660ee59f35026587ca55e2d86d
Drew Daniels <daniels_drew@protonmail.com>
2025-05-26 12:46:43 -0500

    install stylua on deb

--- dot_config/nvim/lua/plugins/dark-notify.lua
+++ dot_config/nvim/lua/plugins/dark-notify.lua
@@ -1,9 +1,12 @@
 return {
 	"cormacrelf/dark-notify",
-	build = function ()
-	  if vm.fn.has("macunix") == 1 then
-	    return "brew install cormacrelf/tap/dark-notify"
-	  end
+	build = function()
+		if vim.fn.has("macunix") == 1 then
+			return "brew install cormacrelf/tap/dark-notify"
+		end
+	end,
+	cond = function()
+		return vim.fn.has("macunix") == 1
 	end,
 	config = function(_, _)
 		require("dark_notify").run({
