7e9cd860d2baf99de15272a874ad894ffe7821a0
Drew Daniels <daniels_drew@protonmail.com>
2025-06-03 20:35:39 -0500

    Revert "enable some LSP capabilities for neorg fts"
    This reverts commit 2f4a2588a06f1a5db9253e59b836c2283223d2ed.

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -1,37 +1,29 @@
 return {
-	"nvim-neorg/neorg",
-	dependencies = { "luarocks.nvim" },
-	lazy = false,
-	version = "*",
-	opts = {
-		load = {
-			["core.defaults"] = {}, -- Loads default behaviour
-			["core.concealer"] = {}, -- Adds pretty icons to your documents
-			["core.dirman"] = { -- Manages Neorg workspaces
-				config = {
-					workspaces = {
-						su = "~/projects/work_notes/su/2024",
-						ooo = "~/projects/work_notes/ooo/2024",
-					},
-				},
-			},
-			["core.keybinds"] = {},
-			["core.export"] = {},
-			["core.presenter"] = {
-				config = {
-					zen_mode = "zen-mode",
-				},
-			},
-			["core.summary"] = {},
-			["core.text-objects"] = {},
-			["core.completion"] = {
-				config = {
-					engine = {
-						module_name = "external.lsp-completion",
-					},
-				},
-			},
-			["external.interim-ls"] = {},
-		},
-	},
+  "nvim-neorg/neorg",
+  dependencies = { "luarocks.nvim" },
+  lazy = false,
+  version = "*",
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
+          workspaces = {
+            su = "~/projects/work_notes/su/2024",
+            ooo = "~/projects/work_notes/ooo/2024",
+          },
+        },
+      },
+      ["core.keybinds"] = {},
+      ["core.export"] = {},
+      ["core.presenter"] = {
+        config = {
+          zen_mode = "zen-mode",
+        },
+      },
+      ["core.summary"] = {},
+      ["core.text-objects"] = {},
+    },
+  },
 }
