e1481a0691f7cb582d067d526497564a9b9e8746
Drew Daniels <drew.daniels@webpt.com>
2025-06-17 08:13:38 -0500

    utility function to upgrade homebrew and misc cleanup steps

---
+++ dot_config/fish/functions/hupgrade.fish
@@ -0,0 +1,5 @@
+function brupgrade -d "Updates homebrew, upgrades homebrew packages, and takes care of some things"
+    command brew update && brew outdated && brew upgrade && brew cleanup
+    # https://github.com/Homebrew/homebrew-core/pull/218854
+    command rm -f (brew --prefix)/etc/gitconfig
+end
