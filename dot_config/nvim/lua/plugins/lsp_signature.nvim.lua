935e605e9bc66ea8b23bc21572f77bcbd4da6ee6
Drew Daniels <drew.daniels@webpt.com>
2025-06-17 15:19:59 -0500

    updates

--- dot_config/nvim/lua/plugins/lsp_signature.nvim.lua
+++ dot_config/nvim/lua/plugins/lsp_signature.nvim.lua
@@ -1,5 +1,15 @@
 return {
 	"ray-x/lsp_signature.nvim",
 	event = "InsertEnter",
-	opts = {},
+	-- Deactivating, because this can be kind of noisy
+	cond = false,
+	opts = {
+		hint_prefix = {
+			above = "↙ ",
+			current = "← ",
+			below = "↖ ",
+		},
+		floating_window = true,
+		hint_enable = false,
+	},
 }
