6807daa99fe62be800b518926192bd8cbc6d3e49
Drew Daniels <drew.daniels@webpt.com>
2025-06-18 08:32:31 -0500

    fix typo

--- dot_config/fish/functions/hupgrade.fish
+++ dot_config/fish/functions/hupgrade.fish
@@ -1,4 +1,4 @@
-function brupgrade -d "Updates homebrew, upgrades homebrew packages, and takes care of some things"
+function hupgrade -d "Updates homebrew, upgrades homebrew packages, and takes care of some things"
     command brew update && brew outdated && brew upgrade && brew cleanup
     # https://github.com/Homebrew/homebrew-core/pull/218854
     command rm -f (brew --prefix)/etc/gitconfig
