1b1963cd494620d445acd0885b89fbf470a7cdef
Drew Daniels <daniels_drew@protonmail.com>
2025-06-18 20:52:43 -0500

    obsidian.nvim config

--- dot_config/nvim/lua/plugins/obsidian.nvim.lua
+++ dot_config/nvim/lua/plugins/obsidian.nvim.lua
@@ -1,5 +1,5 @@
 return {
-	"epwalsh/obsidian.nvim",
+	"obsidian-nvim/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
 	ft = "markdown",
@@ -14,16 +14,38 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	---@module 'obsidian'
+	---@type obsidian.config.ClientOpts
 	opts = {
 		workspaces = {
 			{
-				name = "personal",
-				path = "~/vaults/personal",
+				name = "home",
+				path = "~/vaults/home",
 			},
 			{
 				name = "work",
 				path = "~/vaults/work",
 			},
+		},
+		picker = {
+			name = "fzf-lua",
+			note_mappings = {
+				-- Create a new note from your query.
+				new = "<C-x>",
+				-- Insert a link to the selected note.
+				insert_link = "<C-l>",
+			},
+			tag_mappings = {
+				-- Add tag(s) to current note.
+				tag_note = "<C-x>",
+				-- Insert a tag at the current location.
+				insert_tag = "<C-l>",
+			},
+		},
+		completion = {
+			nvim_cmp = false,
+			blink = true,
+			min_chars = 2,
 		},
 	},
 }
