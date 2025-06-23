53a452d249c8d19d6da8aa64f0324592c242c56c
Drew Daniels <drew.daniels@webpt.com>
2025-06-23 10:24:13 -0500

    disable obsidian.nvim plugin for now

--- dot_config/nvim/lua/plugins/obsidian.nvim.lua
+++ dot_config/nvim/lua/plugins/obsidian.nvim.lua
@@ -4,6 +4,8 @@ return {
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
 	ft = "markdown",
+  -- Disabling this plugin for now until I get vault directory set up working consistently on home and work computers
+  cond = false,
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
 	-- event = {
 	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
