88959335738d6aaf4fcda19ae082ba2df94e70bd
Drew Daniels <drew.daniels@webpt.com>
2025-06-24 13:02:33 -0500

    customize fzf-lua profiles to maximize search performance

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -1,14 +1,15 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    lsp = {
-      async_or_timeout = 10000,
-    },
-    grep = {
-      rg_opts = "--column --line-number --no-heading --color=always --smart-case",
-      RIPGREP_CONFIG_PATH = vim.env.RIPGREP_CONFIG_PATH,
-    },
-  },
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		{ "max-perf", "ivy" },
+		lsp = {
+			async_or_timeout = 10000,
+		},
+		grep = {
+			rg_opts = "--column --line-number --no-heading --color=always --smart-case",
+			RIPGREP_CONFIG_PATH = vim.env.RIPGREP_CONFIG_PATH,
+		},
+	},
 }
