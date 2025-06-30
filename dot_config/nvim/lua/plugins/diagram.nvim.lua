e71fe57a68ae77be117cdc31ef47ea35b30eae64
Drew Daniels <drew.daniels@webpt.com>
2025-06-30 14:34:33 -0500

    additional graph deps, plugin

---
+++ dot_config/nvim/lua/plugins/diagram.nvim.lua
@@ -0,0 +1,29 @@
+return {
+	"3rd/diagram.nvim",
+	dependencies = {
+		"3rd/image.nvim",
+	},
+	config = function()
+		require("diagram").setup({
+			integrations = {
+				require("diagram.integrations.markdown"),
+				require("diagram.integrations.neorg"),
+			},
+			renderer_options = {
+				mermaid = {
+					theme = "forest",
+				},
+				plantuml = {
+					charset = "utf-8",
+				},
+				d2 = {
+					theme_id = 1,
+				},
+				-- gnuplot = {
+				-- 	theme = "dark",
+				-- 	size = "800,600",
+				-- },
+			},
+		})
+	end,
+}
