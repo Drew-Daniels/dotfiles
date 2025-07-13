5a4e66a577aad1e08b15a4f1b0cad1a501204f4e
Drew Daniels <daniels_drew@protonmail.com>
2025-07-13 09:02:16 -0500

    add another config

---
+++ dot_config/nyxt/auto-config.3.lisp
@@ -0,0 +1,24 @@
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/no-script:no-script-mode %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes
+    (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
+
+(defmethod customize-instance ((browser browser) &key)
+  (setf (slot-value browser 'default-cookie-policy) :no-third-party))
+
+(define-configuration browser
+  ((theme theme:+light-theme+)))
