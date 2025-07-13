8d2c2cfbcd8d4470aa9485af0d70bb0ca1353f91
Drew Daniels <daniels_drew@protonmail.com>
2025-07-13 09:16:32 -0500

    move settings

--- dot_config/nyxt/config.lisp
+++ dot_config/nyxt/config.lisp
@@ -1,8 +1,23 @@
-;; "No Script mode - Disables JS on every buffer"
-;; (define-configuration web-buffer
-;;                         ((default-modes
-;;                                (pushnew 'nyxt/mode/no-script:no-script-mode %slot-value%))))
-;; VI-NORMAL-MODE
+;; No tracking
+(define-configuration (web-buffer)
+  ((default-modes
+    (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
+
+;; Disable 3rd party cookies
+(defmethod customize-instance ((browser browser) &key)
+    (setf (slot-value browser 'default-cookie-policy) :no-third-party))
+
+;; Vim keys
 (define-configuration buffer
-                        ((default-modes
-                               (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+  ((default-modes
+   (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+;; Ad Blocker
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+
+;; No JS
+;; (define-configuration (web-buffer)
+;;   ((default-modes (pushnew 'nyxt/mode/no-script:no-script-mode %slot-value%))))
