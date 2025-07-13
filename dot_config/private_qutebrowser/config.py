23fdeafe3392b7749340ad4c728639f375590075
Drew Daniels <daniels_drew@protonmail.com>
2025-07-13 14:44:59 -0500

    disable ruff on qutebrowser config, and add special keybinding to autofill login info

--- dot_config/private_qutebrowser/config.py
+++ dot_config/private_qutebrowser/config.py
@@ -1,3 +1,8 @@
-# TODO: Silence basedpyright and ruff for this file
+# TODO: Silence basedpyright for this file
+
+# NOTE: Disable ruff
+# ruff: noqa
+
 config.load_autoconfig()
 c.fonts.default_family = "JetBrainsMono Nerd Font"
+config.bind("<Ctrl-Shift-u>", "spawn --userscript 1pass.sh")
