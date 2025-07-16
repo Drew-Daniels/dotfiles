da18e151b9306f4d5e07cf7191d7828af3277377
Drew Daniels <daniels_drew@protonmail.com>
2025-07-15 20:59:15 -0500

    use fork of markdown-preview

--- dot_config/nvim/lua/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -1,5 +1,6 @@
 return {
-	"iamcco/markdown-preview.nvim",
+	-- "iamcco/markdown-preview.nvim",
+	"Drew-Daniels/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	build = "cd app && yarn install",
 	init = function()
