01843a57540d4b4b9ef46ba631de40aa33b17007
Drew Daniels <drew.daniels@webpt.com>
2025-07-16 16:27:37 -0500

    add more neovim plugins for debugging

---
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,26 @@
+return {
+	"mfussenegger/nvim-dap",
+	config = function()
+    -- https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(via--codelldb)#ccrust-via-codelldb
+		local dap = require("dap")
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = "codelldb",
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+			},
+		}
+		-- Reuse
+		dap.configurations.c = dap.configurations.cpp
+		dap.configurations.rust = dap.configurations.cpp
+	end,
+}
