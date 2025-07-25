fa2a464c717e6d90b702f5fae30f26cf284431b0
Drew Daniels <drew.daniels@webpt.com>
2025-07-25 09:20:37 -0500

    add js, ts configurations

--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -1,12 +1,24 @@
 return {
 	"mfussenegger/nvim-dap",
 	config = function()
-    -- https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(via--codelldb)#ccrust-via-codelldb
+		-- https://github.com/mfussenegger/nvim-dap/wiki/C-C---Rust-(via--codelldb)#ccrust-via-codelldb
+		-- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
 		local dap = require("dap")
+		-- ADAPTERS
 		dap.adapters.codelldb = {
 			type = "executable",
 			command = "codelldb",
 		}
+		dap.adapters["pwa-node"] = {
+			type = "server",
+			host = "localhost",
+			port = "${port}",
+			executable = {
+				command = "node",
+				args = { os.getenv("HOME") .. "/.config/vscode-js-debug/src/dapDebugServer.js", "${port}" },
+			},
+		}
+		-- CONFIGURATIONS
 		dap.configurations.cpp = {
 			{
 				name = "Launch file",
@@ -22,5 +34,30 @@ return {
 		-- Reuse
 		dap.configurations.c = dap.configurations.cpp
 		dap.configurations.rust = dap.configurations.cpp
+		dap.configurations.javascript = {
+			{
+				type = "pwa-node",
+				request = "launch",
+				name = "Launch file",
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+			},
+		}
+		dap.configurations.typescript = {
+			{
+				type = "pwa-node",
+				request = "launch",
+				name = "Launch file",
+				runtimeExecutable = "deno",
+				runtimeArgs = {
+					"run",
+					"--inspect-wait",
+					"--allow-all",
+				},
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+				attachSimplePort = 9229,
+			},
+		}
 	end,
 }
