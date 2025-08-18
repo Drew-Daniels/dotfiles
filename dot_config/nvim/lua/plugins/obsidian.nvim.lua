0625ac1a2fc614d5e2409ece1e3e4fba14837fa8
Drew Daniels <drew.daniels@webpt.com>
2025-08-18 11:07:24 -0500

    disable typescript formatting, obsidian plugin

--- dot_config/nvim/lua/plugins/obsidian.nvim.lua
+++ dot_config/nvim/lua/plugins/obsidian.nvim.lua
@@ -1,6 +1,7 @@
 return {
 	"obsidian-nvim/obsidian.nvim",
 	-- dev = true,
+	cond = false,
 	version = "*", -- recommended, use latest release instead of latest commit
 	lazy = true,
 	ft = "markdown",
