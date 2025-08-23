1eabcaf0622f6e38a478b718c68f384468f20c59
Drew Daniels <daniels_drew@protonmail.com>
2025-08-23 08:38:22 -0600

    update nvim plugins

--- dot_config/nvim/after/ftplugin/java.lua
+++ dot_config/nvim/after/ftplugin/java.lua
@@ -1,8 +1,5 @@
 local config = {
-	-- cmd = { "/opt/homebrew/bin/jdtls" },
-	-- on Unix
-	-- TODO: See if I can use a function to dynamically get the location of jdtls?
-	cmd = { "/usr/bin/jdtls" },
+	cmd = { "/opt/homebrew/bin/jdtls" },
 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
