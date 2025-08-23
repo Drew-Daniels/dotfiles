660a2531f4bad1d6efdc70634c64610aa9474fc7
Drew Daniels <daniels_drew@protonmail.com>
2025-08-22 19:12:52 -0600

    refactor

--- .install-repositories.sh
+++ .install-repositories.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
+github_base_url="https://github.com/Drew-Daniels"
+codeberg_base_url="https://codeberg.org/drewdaniels"
+
 if [ ! -d ~/projects/friendly-snippets ]; then
-  git clone https://github.com/Drew-Daniels/friendly-snippets.git ~/projects/friendly-snippets
+  git clone "$github_base_url/friendly-snippets.git" ~/projects/friendly-snippets
 fi
 
 if [ ! -d ~/projects/work_notes ]; then
-  git clone https://github.com/Drew-Daniels/work_notes.git ~/projects/work_notes
+  git clone "$github_base_url/work_notes.git" ~/projects/work_notes
 fi
 
 if [ ! -d ~/projects/jg ]; then
-  git clone https://codeberg.org/drewdaniels/jg.git ~/projects/jg
+  git clone "$codeberg_base_url/jg.git" ~/projects/jg
 fi
 
 if [ ! -d ~/projects/dotfiles.wiki ]; then
-  git clone https://github.com/drewdaniels/dotfiles.wiki.git ~/projects/dotfiles.wiki
+  git clone "$github_base_url/dotfiles.wiki.git" ~/projects/dotfiles.wiki
 fi
