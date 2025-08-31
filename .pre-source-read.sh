5664295c87de32ca8ae570aeb9d6a50d6bdbce69
Drew Daniels <daniels_drew@protonmail.com>
2025-08-31 17:25:59 +0000

    remove reference

--- .pre-source-read.sh
+++ .pre-source-read.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 .local/share/chezmoi/.install-directories.sh
-.local/share/chezmoi/.install-repositories.sh
 .local/share/chezmoi/.install-package-manager.sh
 .local/share/chezmoi/.install-password-manager.sh
