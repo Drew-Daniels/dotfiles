473f606b208dfb521e20db60f78ade775b4326b4
Drew Daniels <daniels_drew@protonmail.com>
2025-09-11 14:40:00 -0600

    basic screenshot function

---
+++ dot_config/fish/functions/screenshot.fish
@@ -0,0 +1,17 @@
+function screenshot -d "Takes a screenshot"
+    # TODO: Enable custom dir
+    # TODO: Better checks around file extensions (.jpg, .jpeg, .png, others?)
+
+    # Defaults
+    set -l dir "$HOME/Pictures/Screenshots"
+
+    if test -z "$argv[1]"
+        set -l filename $argv[1]
+        echo "Must specify filename (with extension)"
+        exit 1
+    end
+
+    set -l filename "$argv[1]"
+
+    grim -g "$(slurp)" - | satty -f - -o "$dir/$filename"
+end
