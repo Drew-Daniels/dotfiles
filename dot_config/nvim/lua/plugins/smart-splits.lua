22c1abc136132e45d4a7a675eacf6cb57718886c
Drew Daniels <drew.daniels@webpt.com>
2025-10-13 17:06:05 -0500

    lock smart-splits.nvim to a specific version because later commits introduce a bug

--- dot_config/nvim/lua/plugins/smart-splits.lua
+++ dot_config/nvim/lua/plugins/smart-splits.lua
@@ -1,5 +1,8 @@
 return {
 	"mrjones2014/smart-splits.nvim",
+	-- b65575045cc446833da726a46d447e96c6759c5e seems to have introduced a bug, or at least adds logging that makes it looks like there is a bug:
+	-- tmux init: failed to detect pane_id
+	commit = "c697ea84309db323e4da82d29c1f23304e6910be",
 	opts = {
 		resize_mode = { hooks = { on_leave = require("bufresize").register } },
 	},
