fe8080acae912024024657bb4abcf7c86565fab8
Drew Daniels <drew.daniels@webpt.com>
2025-10-13 16:49:00 -0500

    explicitly set the smart-splits integration before lazy loading is initialized

--- dot_config/nvim/lua/plugins/smart-splits.lua
+++ dot_config/nvim/lua/plugins/smart-splits.lua
@@ -1,6 +1,9 @@
 return {
-  "mrjones2014/smart-splits.nvim",
-  opts = {
-    resize_mode = { hooks = { on_leave = require("bufresize").register } },
-  },
+	"mrjones2014/smart-splits.nvim",
+	opts = {
+		resize_mode = { hooks = { on_leave = require("bufresize").register } },
+	},
+	config = function()
+		vim.g.smart_splits_multiplexer_integration = "tmux"
+	end,
 }
