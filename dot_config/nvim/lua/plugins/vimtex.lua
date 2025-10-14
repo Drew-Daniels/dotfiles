f6aa3235beebe52da9dc3fdf12566259cc08aa7a
Drew Daniels <drew.daniels@webpt.com>
2025-10-14 16:08:44 -0500

    update vimtex config

--- dot_config/nvim/lua/plugins/vimtex.lua
+++ dot_config/nvim/lua/plugins/vimtex.lua
@@ -3,27 +3,13 @@ return {
 	lazy = false, -- we don't want to lazy load VimTeX
 	-- tag = "v2.15", -- uncomment to pin to a specific release
 	init = function()
-		-- VimTeX configuration goes here, e.g.
 		-- vim.g.vimtex_view_method = "zathura"
+		-- NOTE: Using sioyek instead of zathura because it's a more cross-platform friendly alternative
 		vim.g.vimtex_view_method = "sioyek"
 		-- silence warning that tree-sitter is being used for syntax highlighting
 		vim.g.vimtex_syntax_enabled = 0
 		-- NOTE: 'latexmk' is included with texlive by default, and supports automatic re-compilation upon changes
-		vim.g.vimtex_compiler_method = "latexmk"
-		-- NOTE: 'latexmk' defaults below
-		-- vim.g.vimtex_compiler_latexmk = {
-		-- 	aux_dir = "",
-		-- 	out_dir = "",
-		-- 	callback = 1,
-		-- 	continuous = 1,
-		-- 	executable = "latexmk",
-		-- 	hooks = {},
-		-- 	options = {
-		-- 		"-verbose",
-		-- 		"-file-line-error",
-		-- 		"-synctex=1",
-		-- 		"-interaction=nonstopmode",
-		-- 	},
-		-- }
+		-- NOTE: vimtex should use this compiler by default
+		-- vim.g.vimtex_compiler_method = "latexmk"
 	end,
 }
