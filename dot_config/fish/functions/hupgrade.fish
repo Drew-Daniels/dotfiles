fca787cd2fdb0e2a31308992da446eadaa0a5ee9
Drew Daniels <drew.daniels@webpt.com>
2025-10-30 11:19:45 -0500

    update "hupgrade" utility function to automatically remove homebrew node installation if installed

--- dot_config/fish/functions/hupgrade.fish
+++ dot_config/fish/functions/hupgrade.fish
@@ -1,5 +1,11 @@
 function hupgrade -d "Updates homebrew, upgrades homebrew packages, and takes care of some things"
     command brew update && brew outdated && brew upgrade && brew cleanup
+
     # https://github.com/Homebrew/homebrew-core/pull/218854
     command rm -f (brew --prefix)/etc/gitconfig
+
+    if test -d /opt/homebrew/Cellar/node
+        # Many dependencies require node to be installed and will install it implicitly if not found in homebrew installation directory
+        command brew uninstall --ignore-dependencies node
+    end
 end
