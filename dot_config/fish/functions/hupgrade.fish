98238f6241ca77a851cb6ba7e12225b4e125c6ee
Drew Daniels <drew.daniels@webpt.com>
2025-10-31 16:50:21 -0500

    activate aggressive path management setting for mise to ensure mise runtimes are always put first on PATH before homebrew runtimes

--- dot_config/fish/functions/hupgrade.fish
+++ dot_config/fish/functions/hupgrade.fish
@@ -3,13 +3,4 @@ function hupgrade -d "Updates homebrew, upgrades homebrew packages, and takes ca
 
     # https://github.com/Homebrew/homebrew-core/pull/218854
     command rm -f (brew --prefix)/etc/gitconfig
-
-    if test -d /opt/homebrew/Cellar/node
-        # Many dependencies require node to be installed and will install it implicitly if not found in homebrew installation directory
-        brew uninstall --ignore-dependencies node
-    end
-
-    if test -d /opt/homebrew/Cellar/python@3*
-        brew uninstall --ignore-dependencies python3
-    end
 end
