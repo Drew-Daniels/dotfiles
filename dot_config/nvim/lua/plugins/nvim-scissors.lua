db3ccf89e973ef685d13d99fb7778fed998a7b5b
Drew Daniels <drew.daniels@webpt.com>
2025-11-03 14:38:26 -0600

    remove nvim-scissors and update lockfile

--- dot_config/nvim/lua/plugins/nvim-scissors.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "chrisgrieser/nvim-scissors",
-  dependencies = { "nvim-telescope/telescope.nvim", "garymjr/nvim-snippets" },
-  opts = {
-    snippetDir = vim.fn.stdpath("config") .. "/snippets",
-    jsonFormatter = "jq",
-  },
-}
