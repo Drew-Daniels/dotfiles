057564fd9c88b72321397aae2cf44a743fb0aef8
Drew Daniels <daniels_drew@protonmail.com>
2025-11-07 16:49:54 -0600

    remove kde plasma, sddm from nixos machine and ensure sway is always started upon login on nixos

--- dot_config/nixos/configuration.nix
+++ dot_config/nixos/configuration.nix
@@ -82,8 +82,8 @@
   services.xserver.enable = false;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb = {
