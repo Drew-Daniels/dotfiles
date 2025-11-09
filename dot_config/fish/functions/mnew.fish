9cf12d490d54ec62b2c858365bd5dc8273ec3b5e
Drew Daniels <daniels_drew@protonmail.com>
2025-11-09 14:36:19 -0600

    delete unused fish functions

--- dot_config/fish/functions/mnew.fish
+++
@@ -1,3 +0,0 @@
-function mnew -d "Starts a new tmuxinator session, using the name of the current working directory as the session name"
-    tmux new-session -s (basename $PWD)
-end
--- dot_config/fish/functions/mstart.fish
+++
@@ -1,8 +0,0 @@
-function mstart -d "Starts up tmuxinator projects"
-    set -l projects (string split ' ' $DEFAULT_TMUXINATOR_PROJECTS)
-
-    for project in $projects
-        echo "Starting $project"
-        mux start $project --attach=false
-    end
-end
