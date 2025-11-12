9cd9f7d99edc8c2db14123c5598a97944bab528d
Drew Daniels <drew.daniels@webpt.com>
2025-11-12 12:27:42 -0600

    re-enable fish_indent for now

--- dot_config/fish/functions/muxp.fish
+++ dot_config/fish/functions/muxp.fish
@@ -6,6 +6,25 @@ function muxp -d "Starts a new tmux session using the default 'project' tmuxinat
     return 1
   end
 
+  # TODO: Check if first argument provided is a built-in tmuxinator subcommand
+  # If so, pass through to actual `tmux` command
+  # Otherwise, continue with the logic below
+  # TODO: Alias actual `mux` command with this implementation once this is complete so I can use `mux` in the following way:
+  # mux <project-without-custom-tmuxinator-config> - to start or attach to a tmux session with this project name, using the default tmuxinator project configuration or a specific one for this project
+  # mux stop
+  # mux ls
+  # mux ... etc. ...
+  set -l builtin_tmuxinator_cmds "commands" "completions" "copy" "debug" "delete" "doctor" "edit" "implode" "list" "local" "new" "open" "start" "stop" "version"
+
+      # Define a regex pattern
+  #     set regex "^(apple|banana|cherry)$"
+  #
+  #         # Check if the first argument matches the regex pattern
+  #             if string match -q -r "$regex" "$first_argument"
+  #                     echo "Match found: $first_argument matches the pattern."
+  #                             return 0
+  #                                 end
+
   if test -e ~/.config/tmuxinator/$project_name.yml
     mux $project_name
   else
