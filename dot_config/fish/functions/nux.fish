e237fc9cdc81e41b3ee7c9e7e58fcd5a004e957b
Drew Daniels <drew.daniels@webpt.com>
2025-11-16 09:25:53 -0600

    clean up documentation

--- dot_config/fish/functions/nux.fish
+++ dot_config/fish/functions/nux.fish
@@ -6,28 +6,31 @@
 # |__/  |__/ \______/ |__/  \__/
 
 # Features:
-# Enhanced `start` functionality
-# Enhanced `stop` functionality
-# Unified `start`/`stop` interface for all tmux sessions, regardless of whether or not they have a corresponding tmuxinator project configuration created for them
-
+  # Enhanced `start` functionality
+    # Wrapper of `tmuxinator start` subcommand that allows you to start multiple tmux sessions at once by specifying the directory names for all the projects you want to start tmux sessions for
+    # For every directory name listed, if there is a tmuxinator config with the same name, tmuxinator will start a tmux session using that config
+    # Otherwise, tmuxinator will start a tmux session using the default tmuxinator project template, and start the session in that directory
+  # Enhanced `stop` functionality
+    # Wrapper of `tmuxinator stop` subcommand that allows you to stop multiple tmux sessions at once by name
+    # For every session name, if there is a corresponding tmuxinator config with the same name, the tmux session will be stopped with `tmux stop` to ensure that on_project_stop hooks are called
+    # Otherwise, tmux will just kill the session of that name as normal
+  # Unified `start`/`stop` interface for all tmux sessions, regardless of whether or not they have a corresponding tmuxinator project configuration created for them
+  # Enhancement to allow users to start (or attach to) a tmux session with a name matching the current directory, if no arguments are provided
+    # Uses the same start_sessions logic to determine whether or not a project-specific tmuxinator template should be used or the default project template when creating the tmux session
 # NOTE: To use the "wrapped" start functionality, you'll need to have your tmuxinator project configurations use the `attach: false` option, so `nux` can start multiple
 # tmux sessions consecutively without getting attached to one. If you don't do this, you'll get attached to one session, and the next one won't be started until you detach.
 
 # TODO: Enable users to specify alternative `projects` directory
 # TODO: Figure out how to have this command still use fish shell completions for tmuxinator (if possible)
+# TODO: Figure out if there is a way to prevent nested tmux sessions
 
 function nux -d "Wrapper function for tmuxinator (mux) that adds some nice to have functionality"
     set -l first_arg $argv[1]
     set -l stop_regex '^(stop)$'
     set -l builtin_regex '^(commands|completions|copy|cp|debug|delete|doctor|edit|implode|list|ls|local|new|open|start|version)$'
 
-    # Wrapper of `tmuxinator start` subcommand that allows you to start multiple tmux sessions at once by specifying the directory names for all the projects you want to start tmux sessions for
-    # For every directory name listed, if there is a tmuxinator config with the same name, tmuxinator will start a tmux session using that config
-    # Otherwise, tmuxinator will start a tmux session using the default tmuxinator project template, and start the session in that directory
     function start_sessions
         for project in $argv
-            # otherwise create a new tmux session using a specific project template with a name matching the first argument provided, or use default tmuxinator project template
-            # attaches to project tmux session if one already exists
             if test -e ~/.config/tmuxinator/$project.yml
                 tmuxinator $project
             else
@@ -37,9 +40,6 @@ function nux -d "Wrapper function for tmuxinator (mux) that adds some nice to ha
         tmux attach -t $argv[-1]
     end
 
-    # Wrapper of `tmuxinator stop` subcommand that allows you to stop multiple tmux sessions at once by name
-    # For every session name, if there is a corresponding tmuxinator config with the same name, the tmux session will be stopped with `tmux stop` to ensure that on_project_stop hooks are called
-    # Otherwise, tmux will just kill the session of that name as normal
     function stop_sessions
         set -l sessions $argv
         for session in $sessions
@@ -52,38 +52,37 @@ function nux -d "Wrapper function for tmuxinator (mux) that adds some nice to ha
         return 0
     end
 
-    # TODO: Use project-specific tmuxinator config if one exists
+    # `nux`
     # @fish-lsp-disable-next-line 3001
     if test -z $first_arg
         set -l dir (path basename (pwd))
-        tmuxinator project -n $dir d=$dir
-        tmux attach -t $dir
+        start_sessions $dir
         return 0
     end
 
+    # `nux stop-all`
     if string match -q -r '^(stop-all)$' "$first_arg"
         set -l sessions (tmux ls | awk '{print $1}' | sed 's/:$//')
         stop_sessions $sessions
         return 0
     end
 
-    # if stop command passed, special handling is required
-    # if there is a corresponding tmuxinator config, run `mux stop <project>`
-    # otherwise, run `tmux kill-session -t <project>`
+    # `nux stop <project1> <project2> ...`
     if string match -q -r "$stop_regex" "$first_arg"
         set -l sessions $argv[2..-1]
         stop_sessions $sessions
         return 0
     end
 
+    # `nux ls`
     # pass through if another builtin tmuxinator command used
     if string match -q -r "$builtin_regex" "$first_arg"
         tmuxinator $argv
         return 0
     end
 
-    # After verifying first (and potentially, only) argument is not a built-in command, we can assume arguments provided are a list of one or more projects
+    # After verifying first (and potentially, only) argument is not a built-in command, it's assumed the arguments provided are a list of one or more projects
+    # `nux <project1> <project2> ...`
     set -l projects $argv
-
     start_sessions $projects
 end
