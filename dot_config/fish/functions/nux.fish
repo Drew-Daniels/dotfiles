58456ba2a84bdedd3db9a492b3ccfb4a511facd0
Drew Daniels <drew.daniels@webpt.com>
2025-11-16 09:10:19 -0600

    nux documentation

--- dot_config/fish/functions/nux.fish
+++ dot_config/fish/functions/nux.fish
@@ -1,21 +1,29 @@
-function nux -d "Starts a new tmux session using the default 'project' tmuxinator template, and inside the directory in default project directory matching the name provided"
+#  /$$$$$$$  /$$   /$$ /$$   /$$
+# | $$__  $$| $$  | $$|  $$ /$$/
+# | $$  \ $$| $$  | $$ \  $$$$/
+# | $$  | $$| $$  | $$  >$$  $$
+# | $$  | $$|  $$$$$$/ /$$/\  $$
+# |__/  |__/ \______/ |__/  \__/
+
+# Features:
+# Enhanced `start` functionality
+# Enhanced `stop` functionality
+# Unified `start`/`stop` interface for all tmux sessions, regardless of whether or not they have a corresponding tmuxinator project configuration created for them
+
+# NOTE: To use the "wrapped" start functionality, you'll need to have your tmuxinator project configurations use the `attach: false` option, so `nux` can start multiple
+# tmux sessions consecutively without getting attached to one. If you don't do this, you'll get attached to one session, and the next one won't be started until you detach.
+
+# TODO: Enable users to specify alternative `projects` directory
+# TODO: Figure out how to have this command still use fish shell completions for tmuxinator (if possible)
+
+function nux -d "Wrapper function for tmuxinator (mux) that adds some nice to have functionality"
     set -l first_arg $argv[1]
     set -l stop_regex '^(stop)$'
     set -l builtin_regex '^(commands|completions|copy|cp|debug|delete|doctor|edit|implode|list|ls|local|new|open|start|version)$'
 
-    function stop_sessions
-        set -l sessions $argv
-        for session in $sessions
-
-            if test -e ~/.config/tmuxinator/$session.yml
-                tmuxinator stop $session
-            else
-                tmux kill-session -t $session
-            end
-        end
-        return 0
-    end
-
+    # Wrapper of `tmuxinator start` subcommand that allows you to start multiple tmux sessions at once by specifying the directory names for all the projects you want to start tmux sessions for
+    # For every directory name listed, if there is a tmuxinator config with the same name, tmuxinator will start a tmux session using that config
+    # Otherwise, tmuxinator will start a tmux session using the default tmuxinator project template, and start the session in that directory
     function start_sessions
         for project in $argv
             # otherwise create a new tmux session using a specific project template with a name matching the first argument provided, or use default tmuxinator project template
@@ -29,8 +37,22 @@ function nux -d "Starts a new tmux session using the default 'project' tmuxinato
         tmux attach -t $argv[-1]
     end
 
-    # TODO: Figure out how to have this command still use fish shell completions for tmuxinator (if possible)
+    # Wrapper of `tmuxinator stop` subcommand that allows you to stop multiple tmux sessions at once by name
+    # For every session name, if there is a corresponding tmuxinator config with the same name, the tmux session will be stopped with `tmux stop` to ensure that on_project_stop hooks are called
+    # Otherwise, tmux will just kill the session of that name as normal
+    function stop_sessions
+        set -l sessions $argv
+        for session in $sessions
+            if test -e ~/.config/tmuxinator/$session.yml
+                tmuxinator stop $session
+            else
+                tmux kill-session -t $session
+            end
+        end
+        return 0
+    end
 
+    # TODO: Use project-specific tmuxinator config if one exists
     # @fish-lsp-disable-next-line 3001
     if test -z $first_arg
         set -l dir (path basename (pwd))
@@ -39,7 +61,6 @@ function nux -d "Starts a new tmux session using the default 'project' tmuxinato
         return 1
     end
 
-    # TODO: Rename `proj` references to `session` for consistency
     if string match -q -r '^(stop-all)$' "$first_arg"
         set -l sessions (tmux ls | awk '{print $1}' | sed 's/:$//')
         stop_sessions $sessions
@@ -49,10 +70,9 @@ function nux -d "Starts a new tmux session using the default 'project' tmuxinato
     # if stop command passed, special handling is required
     # if there is a corresponding tmuxinator config, run `mux stop <project>`
     # otherwise, run `tmux kill-session -t <project>`
-    # TODO: Add functionality so ALL tmux sessions can be stopped: `nux stop-all`
     if string match -q -r "$stop_regex" "$first_arg"
-        set -l projects $argv[2..-1]
-        stop_sessions $projects
+        set -l sessions $argv[2..-1]
+        stop_sessions $sessions
         return 0
     end
 
