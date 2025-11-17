c78dd2f30110de2f770d555ed8a3e4df0fd9e2a3
Drew Daniels <drew.daniels@webpt.com>
2025-11-16 18:15:20 -0600

    Add .config/nvim/lua/plugins/snacks.nvim.lua Update .config/nvim/lua/plugins/urlview.lua Update .config/nvim/lua/plugins/which-key.lua

--- dot_config/nvim/lua/plugins/urlview.lua
+++ dot_config/nvim/lua/plugins/urlview.lua
@@ -1,6 +1,11 @@
 return {
   "axieax/urlview.nvim",
+  dependencies = {
+    -- NOTE: Not a hard-requirement, but only alternatives are vim.ui.select or Telescope.nvim
+    -- TODO: Create integration with fzf-lua so I can use this as a picker instead
+
+  },
   opts = {
-    default_picker = "fzflua",
+    default_picker = "snacks",
   },
 }
