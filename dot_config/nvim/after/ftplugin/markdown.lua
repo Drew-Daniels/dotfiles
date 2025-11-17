84e17e30b68f4c4b2e6d79cf31076a9b8d291c2d
Drew Daniels <drew.daniels@webpt.com>
2025-11-17 10:37:56 -0600

    fix issue where tabstop and shiftwidth settings were being overridden by neovim markdown file presets

--- dot_config/nvim/after/ftplugin/markdown.lua
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,13 @@
 -- Overrides the setlocal tabstop setting made by /opt/homebrew/Cellar/neovim/0.11.5/share/nvim/runtime/ftplugin/markdown.vim
 -- Or, wherever this file is located
+-- TODO: Remove overrides that are not necessary
+-- autoindent new lines
+vim.bo.autoindent = true
+-- expands tabs into spaces
+vim.bo.expandtab = true
+-- number of spaces to use for each tab
 vim.bo.tabstop = 2
+-- number of spaces to use when indenting
+vim.bo.shiftwidth = 2
+-- number of spaces to use for (auto)indent step
+vim.bo.softtabstop = 2
