39000b6ca69caed6e44157e1842bf6ccded4a44d
Drew Daniels <drew.daniels@webpt.com>
2025-11-16 19:01:08 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/urlview.lua

--- dot_config/nvim/lua/plugins/urlview.lua
+++ dot_config/nvim/lua/plugins/urlview.lua
@@ -1,7 +1,7 @@
 return {
-	"axieax/urlview.nvim",
-	opts = {
-		-- TODO: Create pick integration with fzf-lua/snacks, so I don't have to use vim.ui.select or Telescope.nvim
-		default_picker = "snacks",
-	},
+  "axieax/urlview.nvim",
+  opts = {
+    -- TODO: Create pick integration with fzf-lua/snacks, so I don't have to use vim.ui.select or Telescope.nvim
+    default_picker = "snacks",
+  },
 }
