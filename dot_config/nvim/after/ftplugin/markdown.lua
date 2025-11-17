f917a74cde21794f5de48eada2589e2e5907e2de
Drew Daniels <drew.daniels@webpt.com>
2025-11-17 10:34:59 -0600

    Add .config/nvim/after/ftplugin/markdown.lua Update .config/nvim/lua/plugins/indent-blankline.lua

---
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,3 @@
+-- Overrides the setlocal tabstop setting made by /opt/homebrew/Cellar/neovim/0.11.5/share/nvim/runtime/ftplugin/markdown.vim
+-- Or, wherever this file is located
+vim.bo.tabstop = 2
