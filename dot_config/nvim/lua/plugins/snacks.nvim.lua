cbc24f07a0099396f3ca26f2966e1ce31a674860
Drew Daniels <drew.daniels@webpt.com>
2025-11-19 10:29:54 -0600

    env var to explicitly tell snacks.nvim which term is being used

--- dot_config/nvim/lua/plugins/snacks.nvim.lua
+++ dot_config/nvim/lua/plugins/snacks.nvim.lua
@@ -1,5 +1,17 @@
 return {
 	"folke/snacks.nvim",
 	---@type snacks.Config
-	opts = {},
+	opts = {
+		image = {
+			-- force = true,
+			env = {
+				SNACKS_GHOSTTY = true,
+			},
+			resolve = function(path, src)
+				if require("obsidian.api").path_is_note(path) then
+					return require("obsidian.api").resolve_image_path(src)
+				end
+			end,
+		},
+	},
 }
