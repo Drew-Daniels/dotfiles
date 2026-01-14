e1c39f6fd71f7e72e7377a561e3bdfcf0355128a
Drew Daniels <daniels_drew@protonmail.com>
2026-01-14 13:13:39 -0600

    cleanup

---
+++ dot_config/fish/functions/setup-theme.fish
@@ -0,0 +1,17 @@
+function setup-theme --description "Configure fish theme based on light or dark mode"
+    set -l theme $argv[1]
+    set -l fish_light_theme "zenbones"
+    set -l fish_dark_theme "gruvbox-dark-material"
+
+    if test "$theme" = light
+        set -gx OS_THEME_DARK 0
+        fish_config theme choose "$fish_light_theme"
+        alias jqp="jqp --theme='algol'"
+        set -gx fzf_diff_highlighter delta --paging=never --width=20 --light
+    else
+        set -gx OS_THEME_DARK 1
+        fish_config theme choose "$fish_dark_theme"
+        alias jqp="jqp --theme='gruvbox'"
+        set -gx fzf_diff_highlighter delta --paging=never --width=20 --dark
+    end
+end
