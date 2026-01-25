5d433ab1f3ba7be874495ecdd7edee5c46b6e77f
Drew Daniels <daniels_drew@protonmail.com>
2026-01-25 15:29:01 -0500

    do not highlight color names

--- dot_config/nvim/lua/plugins/nvim-colorizer.lua
+++ dot_config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,8 +1,12 @@
 return {
-  "catgoose/nvim-colorizer.lua",
-  opts = {},
-  -- NOTE: If I want to attach nvim-colorizer to every buffer automatically, use 'config' instead of 'opts'
-  -- config = function ()
-  --   require("colorizer").setup()
-  -- end
+	"catgoose/nvim-colorizer.lua",
+	opts = {
+		user_default_options = {
+			names = false,
+		},
+	},
+	-- NOTE: If I want to attach nvim-colorizer to every buffer automatically, use 'config' instead of 'opts'
+	-- config = function ()
+	--   require("colorizer").setup()
+	-- end
 }
