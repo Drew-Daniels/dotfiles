2752bc694d66e2abc565d731e0b8e174de20b193
Drew Daniels <drew.daniels@webpt.com>
2026-02-26 11:08:22 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/markdown-preview.lua

--- dot_config/nvim/lua/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -1,14 +1,4 @@
 return {
-	-- "iamcco/markdown-preview.nvim",
-	"Drew-Daniels/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	cond = function()
-		-- Only run on macos
-		return vim.fn.has("macunix") == 1
-	end,
-	build = "cd app && yarn install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
+	"selimacerbas/markdown-preview.nvim",
+	dependencies = { "selimacerbas/live-server.nvim" },
 }
