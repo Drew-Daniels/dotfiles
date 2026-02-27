42dd784dd441f47afb55582b386895c1b0f5104c
Drew Daniels <drew.daniels@webpt.com>
2026-02-27 08:37:31 -0600

    resolve conflicts

--- dot_config/nvim/lua/plugins/markdown-preview.lua
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -1,4 +1,9 @@
 return {
 	"selimacerbas/markdown-preview.nvim",
 	dependencies = { "selimacerbas/live-server.nvim" },
+	opts = {
+		-- mermaid_renderer = "rust",
+		mermaid_renderer = "js",
+		scroll_sync = false,
+	},
 }
