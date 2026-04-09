500e36515833f98e2b7515fe81034a5c3d2345d1
Drew Daniels <drew.daniels@webpt.com>
2026-04-09 11:58:19 -0400

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/neorg.lua Update .default-go-packages

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -1,30 +1,31 @@
 return {
-  "nvim-neorg/neorg",
-  dependencies = { "luarocks.nvim" },
-  lazy = false,
-  version = "*",
-  opts = {
-    load = {
-      ["core.defaults"] = {},
-      ["core.concealer"] = {},
-      ["core.dirman"] = {
-        config = {
-          workspaces = {
-            su = "~/projects/work_notes/su/2024",
-            ooo = "~/projects/work_notes/ooo/2024",
-          },
-        },
-      },
-      ["core.keybinds"] = {},
-      ["core.export"] = {},
-      -- NOTE: API likely to be changed soon: https://github.com/nvim-neorg/neorg/wiki/Core-Presenter#overview
-      -- ["core.presenter"] = {
-      --   config = {
-      --     zen_mode = "zen-mode",
-      --   },
-      -- },
-      ["core.summary"] = {},
-      ["core.text-objects"] = {},
-    },
-  },
+	"nvim-neorg/neorg",
+	dependencies = { "luarocks.nvim" },
+	lazy = false,
+	version = "*",
+	cond = false,
+	opts = {
+		load = {
+			["core.defaults"] = {},
+			["core.concealer"] = {},
+			["core.dirman"] = {
+				config = {
+					workspaces = {
+						su = "~/projects/work_notes/su/2024",
+						ooo = "~/projects/work_notes/ooo/2024",
+					},
+				},
+			},
+			["core.keybinds"] = {},
+			["core.export"] = {},
+			-- NOTE: API likely to be changed soon: https://github.com/nvim-neorg/neorg/wiki/Core-Presenter#overview
+			-- ["core.presenter"] = {
+			--   config = {
+			--     zen_mode = "zen-mode",
+			--   },
+			-- },
+			["core.summary"] = {},
+			["core.text-objects"] = {},
+		},
+	},
 }
