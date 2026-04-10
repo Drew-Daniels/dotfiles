b8a19cded120c6202e980b97edbbb52c0566385e
Drew Daniels <drew.daniels@webpt.com>
2026-04-09 23:33:48 -0400

    nux updates

--- dot_config/nvim/lua/plugins/diagram.nvim.lua
+++ dot_config/nvim/lua/plugins/diagram.nvim.lua
@@ -1,5 +1,6 @@
 return {
 	"3rd/diagram.nvim",
+	cond = false,
 	dependencies = {
 		{ "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
 	},
