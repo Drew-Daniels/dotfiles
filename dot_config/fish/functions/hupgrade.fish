75fd36da5bcd22e168f2aa0388ef4dcaaeffcb46
Drew Daniels <daniels_drew@protonmail.com>
2026-05-03 09:24:59 -0400

    Update .config/fish/functions/hupgrade.fish Add .config/opencode/opencode.jsonc Update .zshrc Update scripts/export_secrets.sh

--- dot_config/fish/functions/hupgrade.fish
+++ dot_config/fish/functions/hupgrade.fish
@@ -3,4 +3,7 @@ function hupgrade -d "Updates homebrew, upgrades homebrew packages, and takes ca
 
     # https://github.com/Homebrew/homebrew-core/pull/218854
     command rm -f (brew --prefix)/etc/gitconfig
+
+    brew unlink block-goose-cli
+    ln -s $(brew --prefix)/opt/block-goose-cli/bin/goose $(brew --prefix)/bin/goose-cli
 end
