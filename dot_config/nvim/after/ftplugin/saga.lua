68a7415060da83e799287ae3b0f8ea09ec83ac10
Drew Daniels <daniels_drew@protonmail.com>
2026-06-22 20:19:42 -0400

    saga lsp

--- dot_config/nvim/after/ftplugin/saga.lua
+++ dot_config/nvim/after/ftplugin/saga.lua
@@ -4,3 +4,9 @@ vim.treesitter.language.add("saga", {
 vim.treesitter.start(0, "saga")
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+vim.lsp.start({
+  name = "saga",
+  cmd = { vim.fn.expand("~/projects/towncrier-v2/towncrier"), "saga", "lsp" },
+  root_dir = vim.fs.dirname(vim.fs.find("saga", { upward = true, path = vim.fn.expand("%:p:h") })[1]),
+})
